92e1b166fcfd0bac1efb153da83152a5c59722c9
Andrew Simms <simms.andrew@gmail.com>
2016-08-24 20:22:20 -0600

    Ui: Add Logo and Settings button to Branding section.
    Adding images proved to be more difficult than expected. Ideally these images
    are resized dynamically based on screen size, but this may not be possible
    with react native.
    
    The settings button activates a `Modal` which contains the `Settings`
    component.

--- src/Branding.js
+++ src/Branding.js
@@ -1,26 +1,92 @@
+import { Dimensions, Image, Modal, Text, TouchableHighlight, View } from 'react-native';
 import React, { Component, PropTypes } from 'react';
-import { Text, View } from 'react-native';
 
+import Settings from './Settings'
 import {styles} from './Styles'
 
 export default class Branding extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      modalVisible: false,
+    };
+  }
+
+  setModalVisible(visible){
+    this.setState({modalVisible: visible});
+  }
+
   static propTypes = {
     title: React.PropTypes.string,
   }
 
   static defaultProps = {
-    title: "Bevegram"
+    title: "Does this work?"
   }
 
   render() {
-    return(
-      <View style={[{
+    return (
+      <View
+      style={[{
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }, styles.bevColorPrimary]}>
-        <Text>{this.props.title}</Text>
+      }, styles.bevColorPrimary]}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'flex-start',
+          }}
+        >
+          <Image
+            source={require('../img/logos/main-logo.png')}
+            style={{
+              flex: 1,
+              margin: 10,
+              resizeMode: 'contain',
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'flex-end',
+          }}
+        >
+          <TouchableHighlight
+            onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}
+          >
+            <Image
+              source={require('../img/icons/settings-icon.png')}
+              style={{
+                flex: 1,
+                resizeMode: 'contain',
+                margin: 10,
+              }}
+            />
+          </TouchableHighlight>
+        </View>
+        <Modal
+          animationType={"fade"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {}}
+        >
+          <View
+            style={{flex: 1}}
+          >
+            <TouchableHighlight
+              onPress={() => {
+                this.setModalVisible(!this.state.modalVisible)
+              }}
+            >
+              <Text>Close</Text>
+            </TouchableHighlight>
+            <Settings />
+          </View>
+        </Modal>
       </View>
     );
   }
---
+++ src/Settings.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Settings extends Component {
+  render() {
+    return(
+      <View
+        style={{
+          flex: 1,
+            alignItems: 'center',
+      }}
+      >
+        <Text>Settings coming soon!</Text>
+      </View>
+    );
+  }
+}
