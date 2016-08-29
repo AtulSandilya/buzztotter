43826633552ba3deb55ec7cd61348acd92b18e89
Andrew Simms <simms.andrew@gmail.com>
2016-08-28 23:44:19 -0600

    Ui: Add styled dummy Settings.

--- src/Settings.js
+++ src/Settings.js
@@ -1,17 +1,117 @@
 import React, { Component, PropTypes } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+
+import TitleText from './TitleText'
+
+import {colors} from './Styles'
+
+const styles = StyleSheet.create({
+  settingLine: {
+      flex: 1,
+      flexDirection: 'row',
+      borderBottomWidth: 1,
+      borderColor: colors.subtleSeparator,
+      marginBottom: 20,
+  }
+});
 
 export default class Settings extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      notifications: true,
+      location: true,
+    }
+  }
+
+  toggleSetting(keyValue){
+    var updateObject = {};
+    updateObject[keyValue] = !this.state[keyValue];
+    this.setState(updateObject);
+  }
+
   render() {
     return(
       <View
         style={{
           flex: 1,
-            alignItems: 'center',
-      }}
+          padding: 20,
+        }}
       >
-        <Text>Settings coming soon!</Text>
+        <View>
+          <TitleText title="Settings" />
+        </View>
+        <SettingLine>
+          <SettingLeft>
+            <SettingName>Notifications:</SettingName>
+          </SettingLeft>
+          <SettingRight>
+            <Switch
+              onValueChange={(value) => this.toggleSetting('notifications')}
+              value={this.state.notifications}
+            />
+          </SettingRight>
+        </SettingLine>
+        <SettingLine>
+          <SettingLeft>
+            <SettingName>Location:</SettingName>
+          </SettingLeft>
+          <SettingRight>
+            <Switch
+              onValueChange={(value) => this.toggleSetting('location')}
+              value={this.state.location}
+            />
+          </SettingRight>
+        </SettingLine>
       </View>
+    );
+  }
+}
+
+class SettingLeft extends Component {
+  render() {
+    return(
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'flex-start',
+        }}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+class SettingRight extends Component {
+  render() {
+    return(
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'flex-end',
+        }}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+class SettingLine extends Component {
+  render() {
+    return(
+      <View style={styles.settingLine}>
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+class SettingName extends Component {
+  render() {
+    return(
+      <Text style={{fontSize: 20, fontWeight: 'bold'}}>{this.props.children}</Text>
     );
   }
 }
