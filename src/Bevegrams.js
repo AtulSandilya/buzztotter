58d4f2a0f5551210326d90fbeaf0e2b25dfb8151
Andrew Simms <simms.andrew@gmail.com>
2016-09-02 14:46:47 -0600

    Feature: Add skeleton list view for Bevegrams.

--- src/Bevegrams.js
+++ src/Bevegrams.js
@@ -1,17 +1,37 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
+
+import {globalStyles} from './Global';
 
 export default class Bevegrams extends Component {
   render() {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     return(
       <View style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
       }}>
-        <Text>
-          No one has bought you a drink yet :(
-        </Text>
+        <ListView
+          dataSource={ds.cloneWithRows([
+            {
+              from: "Andrew Simms",
+              message: "Have a cold one on me!",
+              date: "May 7 2016"
+            },
+            {
+              from: "Dave Simms",
+              message: "Have a cold one on me!",
+              date: "May 7 2016"
+            },
+          ])}
+          renderRow={(rowData) =>
+            <View>
+              <Text>{rowData.from}</Text>
+              <Text>{rowData.message}</Text>
+              <Text>{rowData.date}</Text>
+            </View>
+          }
+          renderSeparator={(sectionId, rowId) => <View key={rowId} style={globalStyles.listRowSeparator} />}
+        />
       </View>
     );
   }
