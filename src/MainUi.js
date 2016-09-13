dc290532ecf9cbc8cf943ce5ee0d5ae2c8cd15cb
Andrew Simms <simms.andrew@gmail.com>
2016-09-12 19:02:55 -0600

    Refactor: Convert `MainNavButtons` to stateless component.

--- src/MainUi.js
+++ src/MainUi.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 import {connect} from 'react-redux';
 
 import CBranding from './containers/CBranding';
-import MainNavButtons from './MainNavButtons';
+import MainNavButtons from './components/MainNavButtons';
 import MainViewPager from './MainViewPager';
 
 class MainUi extends Component {
---
+++ src/components/MainNavButtons.js
@@ -0,0 +1,52 @@
+import { StyleSheet, View } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+
+import CMainNavButton from '../containers/CMainNavButton'
+
+import {sceneKeys} from '../reducers/view';
+
+import {colors} from '../Styles'
+
+const styles = StyleSheet.create({
+    navContainer: {
+        flex: 1,
+        flexDirection: 'row',
+    }
+});
+
+const MainNavButtons = () => (
+  <View style={styles.navContainer}>
+    <CMainNavButton
+      label="Contacts"
+      sceneKey={sceneKeys.contacts}
+    />
+    <MainNavSeparator />
+    <CMainNavButton
+      label="Bevegrams"
+      sceneKey={sceneKeys.bevegrams}
+    />
+    <MainNavSeparator />
+    <CMainNavButton
+      label="Map"
+      sceneKey={sceneKeys.bevegramLocations}
+    />
+    <MainNavSeparator />
+    <CMainNavButton
+      label="History"
+      sceneKey={sceneKeys.history}
+    />
+  </View>
+)
+
+const MainNavSeparator = () => (
+  <View
+    style={{
+      backgroundColor: colors.bevActiveSecondary,
+      opacity: 50,
+      alignSelf: 'stretch',
+      width: 1,
+    }}
+  />
+)
+
+export default MainNavButtons;
