bccc315ace2b394cfe231ddd3936b0c86f8a37f0
Andrew Simms <simms.andrew@gmail.com>
2016-09-30 22:51:35 -0600

    Feature: Save the app state in persistent storage.

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,14 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import {enableBatching} from 'redux-batched-actions';
+import * as persistentStorage from 'redux-storage';
+import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
 
 import appReducers from './reducers';
 import rootSaga from './sagas/index';
 
+const engine = createEngine('BevStorage');
+const storageMiddleware = persistentStorage.createMiddleware(engine);
+const storageReducer = persistentStorage.reducer(appReducers);
+
 const sagaMiddleware = createSagaMiddleware();
 
 function configureStore(reducers){
-  let store = createStore(enableBatching(reducers), applyMiddleware(sagaMiddleware));
+  let store = createStore(enableBatching(reducers), applyMiddleware(
+    sagaMiddleware,
+    storageMiddleware,
+  ));
   sagaMiddleware.run(rootSaga);
 
   if(module.hot){
@@ -21,5 +30,9 @@ function configureStore(reducers){
   return store;
 }
 
-let store = configureStore(appReducers)
+let store = configureStore(storageReducer);
+
+const load = persistentStorage.createLoader(engine);
+load(store);
+
 export default store;
