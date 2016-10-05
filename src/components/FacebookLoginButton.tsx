a4188d9d1b71df61a8a41877be229154b5ffd247
Andrew Simms <simms.andrew@gmail.com>
2016-10-05 00:48:37 -0600

    Typescript: Add interface for `FacebookLoginButton`.

--- src/components/FacebookLoginButton.tsx
+++ src/components/FacebookLoginButton.tsx
@@ -3,7 +3,12 @@ import { Component, PropTypes } from 'react';
 
 import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
-const FacebookLoginButton = ({loginDispatch = () => {}, logoutDispatch = () => {}}) => (
+interface FacebookLoginButtonProps {
+  loginDispatch?(token: string): void;
+  logoutDispatch?(): void;
+}
+
+const FacebookLoginButton: React.StatelessComponent<FacebookLoginButtonProps> = ({loginDispatch, logoutDispatch}) => (
   <LoginButton
     readPermissions={['public_profile', 'user_friends', 'email']}
     onLoginFinished={(error, result) => {
@@ -14,13 +19,17 @@ const FacebookLoginButton = ({loginDispatch = () => {}, logoutDispatch = () => {
       } else {
         AccessToken.getCurrentAccessToken().then(
           (data) => {
-            loginDispatch(data.accessToken.toString());
+            if(loginDispatch){
+              loginDispatch(data.accessToken.toString());
+            }
           }
         )
       }
     }}
     onLogoutFinished={() => {
-      logoutDispatch();
+      if(logoutDispatch){
+        logoutDispatch();
+      }
     }}
   />
 )
