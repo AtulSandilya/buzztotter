10f615d07497acec3363cf7b9bebfcb46cef20b4
Andrew Simms <simms.andrew@gmail.com>
2016-10-10 23:37:32 -0600

    Test: add app reducer test.

--- __tests__/reducers/app-test.js
+++ __tests__/reducers/app-test.js
@@ -1,14 +1,20 @@
 import {app} from '../../build/reducers/app';
 
 describe('app reducer', () => {
+  const initialState = app(undefined, {});
+
   it('should return the initial state', () => {
-    expect(app(undefined, {})).toEqual({
+    expect(initialState).toEqual({
       isLoading: true,
     })
   })
 
+  it('should ignore other actions', () => {
+      expect(app(initialState, {type: 'OTHER_ACTION'})).toEqual(initialState);
+  })
+
   it('should handle LOADING_COMPLETE', () => {
-    expect(app({}, {type: 'LOADING_COMPLETE'})).toEqual({
+    expect(app(initialState, {type: 'LOADING_COMPLETE'})).toEqual({
       isLoading: false,
     })
   })
