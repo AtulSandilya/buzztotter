6034ce1421421a47eca8a0023f4e6b1c76447175
Andrew Simms <simms.andrew@gmail.com>
2016-10-18 00:01:02 -0600

    Test: Use snapshots for `Contact` test.

--- __tests__/components/Contact-test.js
+++ __tests__/components/Contact-test.js
@@ -4,23 +4,26 @@ import Contact from '../../build/components/Contact';
 
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Contact
-      name={{first: "Andrew", last: "Simms"}}
-      birthday="12/12"
-      imagePath="test.jpg"
-      openPurchaseModal={() => {}}
-      closePurchaseModal={() => {}}
-    />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('Contact component', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Contact
+        name={{first: "Andrew", last: "Simms"}}
+        birthday="12/12"
+        imagePath="test.jpg"
+        openPurchaseModal={() => {}}
+        closePurchaseModal={() => {}}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-// it('fails', () => {
-//   const tree = renderer.create(
-//     <Contact
-//     />
-//   ).toJSON();
-//   expect(tree).toThrowErrorMatchingSnapshot();
-// });
+  it('fails without props', () => {
+    expect(() => {
+      renderer.create(
+        <Contact/>
+      ).toJSON();
+    }).toThrow();
+  });
+})
+
