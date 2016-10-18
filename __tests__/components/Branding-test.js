888069a7e4eb1ad6de5510d8fe2795e82349eeec
Andrew Simms <simms.andrew@gmail.com>
2016-10-17 21:42:28 -0600

    Test: `Branding` component.

---
+++ __tests__/components/Branding-test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { shallow } from 'enzyme';
+
+import Branding from '../../build/components/Branding';
+
+jest.mock('react-native-fbsdk', () => '');
+
+describe('Branding component', () => {
+  it('renders with no bevegrams', () => {
+    const wrapper = shallow(
+      <Branding
+        bevegramsList={[]}
+        closeModal={(input) => {return input}}
+      />
+    )
+
+    // Branding container view has 3 children
+    expect(wrapper.find(View).length).toEqual(3);
+  })
+})
+
