69dda98bf3720d85c290b7e532268bacd19e714d
Andrew Simms <simms.andrew@gmail.com>
2016-10-26 12:01:16 -0600

    Feature: Style navBar to match other styles.
    The back icon color is fixed (it is an image) so this will have to wait.

--- src/routers/InitialRouter.tsx
+++ src/routers/InitialRouter.tsx
@@ -8,15 +8,32 @@ import { Actions, Router, Scene } from 'react-native-router-flux';
 
 import store from '../configureStore';
 
+import {globalColors} from '../components/GlobalStyles';
+
 import MainUi from '../components/MainUi';
 import CLogin from '../containers/CLogin';
+import CPurchaseBeer from '../containers/CPurchaseBeer';
 
 const scenes = (showLogin) => {
   return (
     Actions.create(
       <Scene key="root" hideNavBar={true}>
         <Scene key="loginScene" hideNavBar={true} component={CLogin} initial={showLogin}/>
-        <Scene key="mainScene" hideNavBar={true} component={MainUi} panHandlers={null} initial={!showLogin}/>
+        <Scene key="MainUi" hideNavBar={true} component={MainUi} panHandlers={null} initial={!showLogin}/>
+        <Scene
+          key="PurchaseBeer"
+          title="Purchase Beer"
+          hideNavBar={false}
+          component={CPurchaseBeer}
+          navigationBarStyle={{
+            backgroundColor: globalColors.bevPrimary,
+          }}
+          titleStyle={{
+            color: '#ffffff',
+          }}
+          backTitle="Contacts"
+          backButtonTextStyle={{color: '#ffffff'}}
+        />
       </Scene>
     )
   )
