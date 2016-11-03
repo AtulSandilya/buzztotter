f80740218ecd21e9189ab96a07180c20efcbff3c
Andrew Simms <simms.andrew@gmail.com>
2016-11-03 17:40:40 -0600

    Feature: Improve credit card validation.
    More throughly validate each input.
    
    Visually show the user where the error occurred.

--- src/components/AddCreditCard.tsx
+++ src/components/AddCreditCard.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Component, PropTypes } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import moment from 'moment';
 
 import {isIOS} from '../Utilities';
 
@@ -16,6 +18,10 @@ interface AddCreditCardState {
   cardExpMonth: string;
   cardExpYear: string;
   cardCvc: string;
+  showCardNumberAsError: boolean;
+  showCardExpMonthAsError: boolean;
+  showCardExpYearAsError: boolean;
+  showCardCvcAsError: boolean;
 }
 
 export interface AddCreditCardProps {
@@ -33,35 +39,87 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
       cardExpMonth: "",
       cardExpYear: "",
       cardCvc: "",
+      showCardNumberAsError: false,
+      showCardExpMonthAsError: false,
+      showCardExpYearAsError: false,
+      showCardCvcAsError: false,
     }
   }
 
-  isValidCardNumber(cardNum) {
-    if(cardNum.length !== 16){
-      this.creditCardErrorAlert("Invalid card number!");
+  verifyCard(){
+    if(this.clientSideVerify()) {
+      alert("Verification Complete");
+    }
+  }
+
+  clientSideVerify(): boolean{
+    return this.isValidCardNumber(this.getCardNumber())
+           && this.isValidCardExpMonth(this.state.cardExpMonth)
+           && this.isValidCardExpYear(this.state.cardExpYear)
+           && this.isValidCardCvc(this.state.cardCvc);
+  }
+
+  getCardNumber(): string {
+    return this.state.cardNum1 + this.state.cardNum2 + this.state.cardNum3 + this.state.cardNum4;
+  }
+
+  isValidCardNumber(cardNum: string): boolean {
+    if(isNaN(parseInt(cardNum)) || cardNum.length !== 16){
+      this.creditCardErrorAlert("Invalid Card Number");
+      this.updateState("showCardNumberAsError", true);
       return false;
     }
+
+    this.updateState("showCardNumberAsError", false);
     return true;
   }
 
-  isValidCardExp(cardExp){
-    if(cardExp.length < 1){
-      this.creditCardErrorAlert("Invalid expiration date!");
-      return false;
+  isValidCardExpMonth(cardExpMonth: string): boolean{
+    // parseInt returns NaN if cardExpMonth is not an int. NaN fails the
+    // comparisons in the if statement.
+    const month = parseInt(cardExpMonth);
+    if(month >= 1 && month <= 12){
+      this.updateState('showCardExpMonthAsError', false);
+      return true;
     }
-    return true;
+
+    this.updateState('showCardExpMonthAsError', true);
+    this.creditCardErrorAlert("Invalid Expiration Month");
+    return false;
   }
 
-  isValidCardCvc(cvc){
-    if(cvc.length !== 3){
-      this.creditCardErrorAlert("Invalid CVC!");
+  isValidCardExpYear(cardExpYear: string): boolean{
+    const thisYear: number = new Date().getFullYear();
+    // The user input year is two digit and must be converted to 4 digits.
+    // This is done by rounding the current year to the lowest hundred and
+    // adding the year
+    const year: number = parseInt(cardExpYear) + (Math.floor(thisYear  / 100) * 100);
+    const validYearVariance: number = 20;
+
+    if(year >= thisYear && year <= thisYear + validYearVariance){
+      this.updateState('showCardExpYearAsError', false);
+      return true;
+    }
+
+    this.updateState('showCardExpYearAsError', true);
+    this.creditCardErrorAlert("Invalid Expiration Year");
+    return false;
+  }
+
+  isValidCardCvc(cvc: string): boolean{
+    const cvcInt: number = parseInt(cvc)
+    if(cvc.length !== 3 || isNaN(cvcInt)){
+      this.updateState('showCardCvcAsError', true);
+      this.creditCardErrorAlert("Invalid CVC");
       return false;
     }
+
+    this.updateState('showCardCvcAsError', false);
     return true;
   }
 
   creditCardErrorAlert(message){
-    alert("Credit Card Error: " + message);
+    Alert.alert("Error Processing Credit Card:", message + "!");
   }
 
   updateState(property, value){
@@ -92,7 +150,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                 placeholder="1234"
                 width={40}
                 returnKeyType="next"
+                showError={this.state.showCardNumberAsError}
                 onChangeText={(text) => {
+                  this.updateState("showCardNumberAsError", false);
                   this.updateState("cardNum1", text);
                 }}
               />
@@ -102,7 +162,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                 maxChars={4}
                 placeholder="5678"
                 width={40}
+                showError={this.state.showCardNumberAsError}
                 onChangeText={(text) => {
+                  this.updateState("showCardNumberAsError", false);
                   this.updateState("cardNum2", text);
                 }}
               />
@@ -112,7 +174,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                 maxChars={4}
                 placeholder="1234"
                 width={40}
+                showError={this.state.showCardNumberAsError}
                 onChangeText={(text) => {
+                  this.updateState("showCardNumberAsError", false);
                   this.updateState("cardNum3", text);
                 }}
               />
@@ -122,7 +186,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                 maxChars={4}
                 placeholder="5678"
                 width={40}
+                showError={this.state.showCardNumberAsError}
                 onChangeText={(text) => {
+                  this.updateState("showCardNumberAsError", false);
                   this.updateState("cardNum4", text);
                 }}
               />
@@ -152,7 +218,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                   maxChars={2}
                   placeholder="01"
                   width={30}
+                  showError={this.state.showCardExpMonthAsError}
                   onChangeText={(text) => {
+                  this.updateState("showCardExpMonthAsError", false);
                     this.updateState("cardExpMonth", text);
                   }}
                 />
@@ -169,7 +237,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                   maxChars={2}
                   placeholder="20"
                   width={30}
+                  showError={this.state.showCardExpYearAsError}
                   onChangeText={(text) => {
+                    this.updateState("showCardExpYearAsError", false);
                     this.updateState("cardExpYear", text);
                   }}
                 />
@@ -191,7 +261,9 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
                 maxChars={3}
                 placeholder="123"
                 width={45}
+                showError={this.state.showCardCvcAsError}
                 onChangeText={(text) => {
+                  this.updateState("showCardCvcAsError", false);
                   this.updateState("cardCvc", text);
                 }}
                 returnKeyType="done"
@@ -210,7 +282,7 @@ export default class AddCreditCard extends Component<AddCreditCardProps, AddCred
             <View style={globalStyles.bevLineRight}>
               <BevButton
                 buttonText="Verify Card"
-                bevButtonPressed={() => alert("Verify")}
+                bevButtonPressed={this.verifyCard.bind(this)}
                 buttonFontSize={20}
                 margin={0}
               />
@@ -227,6 +299,7 @@ interface CreditCardInputProps {
   width: number;
   maxChars: number;
   placeholder: string;
+  showError?: boolean;
   returnKeyType?: "next" | "done";
   onChangeText(string): void;
 }
@@ -243,6 +316,7 @@ class CreditCardInput extends Component<CreditCardInputProps, CreditCardInputSta
     placeholder: "",
     onChangeText: undefined,
     returnKeyType: "next",
+    showError: false,
   }
 
   onSubmit() {
@@ -253,21 +327,29 @@ class CreditCardInput extends Component<CreditCardInputProps, CreditCardInputSta
 
   render() {
     return(
-      <TextInput
-        style={{
-          width: this.props.width,
-          textAlign: 'center',
-          height: 45,
-        }}
-        ref="textInput"
-        keyboardType="numeric"
-        returnKeyType={this.props.returnKeyType}
-        maxLength={this.props.maxChars}
-        placeholder={this.props.placeholder}
-        placeholderTextColor={"#cccccc"}
-        onChangeText={this.props.onChangeText}
-        onSubmitEditing={this.onSubmit.bind(this)}
-      />
+      <View style={ this.props.showError ? {
+        borderBottomWidth: 1,
+        borderColor: 'rgba(255, 0, 0, 0.75)',
+      } : {
+        borderBottomWidth: 1,
+        borderColor: 'rgba(255, 255, 255, 1)',
+      }}>
+        <TextInput
+          style={{
+            width: this.props.width,
+            textAlign: 'center',
+            height: 45,
+          }}
+          ref="textInput"
+          keyboardType="numeric"
+          returnKeyType={this.props.returnKeyType}
+          maxLength={this.props.maxChars}
+          placeholder={this.props.placeholder}
+          placeholderTextColor={"#cccccc"}
+          onChangeText={this.props.onChangeText}
+          onSubmitEditing={this.onSubmit.bind(this)}
+        />
+      </View>
     );
   }
 }
