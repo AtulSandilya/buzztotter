d9038544d107f85cdcef03ac62ee1361cb02156b
Andrew Simms <simms.andrew@gmail.com>
2016-11-21 13:03:09 -0700

    Feature: `TextInput` scrolls up when blocked by keyboard.

--- src/components/RouteWithNavBarWrapper.tsx
+++ src/components/RouteWithNavBarWrapper.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Component, PropTypes } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
 import {isIOS, isAndroid} from '../Utilities';
 
 interface RouteWithNavBarWrapperProps {
@@ -11,10 +13,10 @@ interface RouteWithNavBarWrapperProps {
 const RouteWithNavBarWrapper: React.StatelessComponent<RouteWithNavBarWrapperProps> = ({children}) => {
   const topMargin = isIOS ? 64 : isAndroid ? 54 : 0
   return (
-    <ScrollView style={{flex: 1}}>
+    <KeyboardAwareScrollView style={{flex: 1}}>
       <View style={{height: topMargin}} />
         {children}
-    </ScrollView>
+    </KeyboardAwareScrollView>
   )
 }
 
