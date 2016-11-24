6f83613f30092f4bc39f2323c146758e08886c46
Andrew Simms <simms.andrew@gmail.com>
2016-11-24 00:46:56 -0700

    Refactor: Cache window sizes in `Utilities`.

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -1,9 +1,15 @@
 import {
   Dimensions,
   Platform,
+  StatusBar,
 } from 'react-native';
 
 export const isAndroid = Platform.OS === 'android';
 export const isIOS  = Platform.OS === 'ios';
 
-export const isNarrow = Dimensions.get("window").width <= 320;
+const {height, width} = Dimensions.get("window")
+export const WindowHeight = height;
+export const WindowWidth = width;
+export const isNarrow = WindowWidth <= 320;
+
+export const StatusBarHeight = isIOS ? 20 : (isAndroid ? StatusBar.currentHeight : 20);
