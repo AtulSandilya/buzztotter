91b8241f20641fd955e700e7692dad51b95abe04
Andrew Simms <simms.andrew@gmail.com>
2016-12-05 17:21:49 -0700

    Android: Use a reasonable `elevation` on `BevButton`

--- src/components/BevButton.tsx
+++ src/components/BevButton.tsx
@@ -42,7 +42,7 @@ const styles = StyleSheet.create<Style>({
       width: 2,
       height: 1,
     },
-    elevation: 50,
+    elevation: 5,
   },
   greenButton: {
     backgroundColor: globalColors.bevPrimary,
