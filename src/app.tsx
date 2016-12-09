3fdf07e35aa74545c8eeecf12ac964c73651e6a5
Andrew Simms <simms.andrew@gmail.com>
2016-12-08 23:54:48 -0700

    Refactor: Cleanup dead notifications code.

--- src/app.tsx
+++ src/app.tsx
@@ -7,15 +7,12 @@ import {
 } from 'react-native';
 
 import { Provider } from 'react-redux'
-import {batchActions} from 'redux-batched-actions';
 
 import {isAndroid} from './Utilities';
 
 import CInitialRouter from './containers/CInitialRouter.js';
 
 import store from './configureStore';
-
-import {modalKeys} from './reducers/modals';
 
 export default class Bevegram extends Component<{}, {}> {
   constructor(props){
@@ -27,10 +24,10 @@ export default class Bevegram extends Component<{}, {}> {
       PushNotification.configure({
         // When notification is clicked
         onNotification: function(notification) {
-          store.dispatch(batchActions([
-            {type: 'CLOSE_MODAL', modalKey: modalKeys.settingsModal},
-            {type: 'GOTO_VIEW', newPosition: 1},
-          ]))
+          store.dispatch({type: "GO_TO_ROUTE", payload: {
+            route: "SendBevegram",
+            // TODO: Get data for sending,
+          }});
         }
       });
     }
