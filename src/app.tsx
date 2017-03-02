cf3699a8d61c929eefe81d2082c540a631b0d68a
Andrew Simms <simms.andrew@gmail.com>
2017-03-02 00:10:27 -0700

    Refactor: Move notification receivers into `MainTabView`
    Storing the fcm token requires the user to be logged in to firebase.

--- src/app.tsx
+++ src/app.tsx
@@ -8,21 +8,15 @@ import {
 
 import { Provider } from 'react-redux'
 
-import {isAndroid} from './Utilities';
-
 import CInitialRouter from './containers/CInitialRouter.js';
 
 import store from './configureStore';
-import {NotificationActions} from './api/notifications';
-import {sceneOrder, sceneKeys} from './reducers/view';
 
 interface BevegramState {
   store: any;
 }
 
 export default class Bevegram extends Component<{}, BevegramState> {
-  notificationListener: any;
-  refreshTokenListener: any;
 
   constructor(props) {
     super(props);
@@ -32,85 +26,6 @@ export default class Bevegram extends Component<{}, BevegramState> {
     }
   }
 
-  storeFCMToken(token) {
-    store.dispatch({type: 'UPDATE_FCM_TOKEN', payload: {
-      fcmToken: token
-    }});
-  }
-
-  onNotificationClick(payload) {
-    switch(payload.action) {
-      case NotificationActions.ShowNewReceivedBevegrams:
-        this.showNewReceivedBevegrams();
-      case NotificationActions.ShowUpcomingBirthdays:
-        store.dispatch({type: 'SEND_BEVEGRAM_TO_CONTACT_VIA_NOTIFICATION', payload: {
-          facebookId: payload.facebookId,
-        }})
-      default:
-        return;
-    }
-  }
-
-  showNewReceivedBevegrams() {
-    store.dispatch({type: 'GO_TO_ROUTE', payload: {
-      route: "MainUi",
-    }});
-    store.dispatch({type: 'GOTO_VIEW', newScene: sceneOrder[sceneKeys.bevegrams]});
-  }
-
-  componentDidMount() {
-    if(isAndroid){
-      const FCM = require('react-native-fcm');
-      FCM.requestPermissions();
-      FCM.getFCMToken().then((token) => {
-        this.storeFCMToken(token);
-      })
-
-      this.notificationListener = FCM.on('notification', (payload) => {
-        // There are 3 notification situations to handle here
-        // 1. App is in the foreground
-        // 2. App is in the background and notification clicked
-        // (payload.opened_from_tray is 1)
-        // 3. App is not running (caught in FCM.getInitialNotification)
-
-        if(payload.open_from_tray === 1){
-          this.onNotificationClick(payload);
-        } else {
-          switch(payload.action) {
-            case NotificationActions.ShowNewReceivedBevegrams:
-              store.dispatch({type: 'ADD_RECEIVED_BEVEGRAM_TO_BADGE', payload: {
-                // Should this badge show the number of bevegrams received or
-                // quantity: payload.quantity,
-                quantity: 1,
-              }})
-              store.dispatch({type: 'FETCH_RECEIVED_BEVEGRAMS'});
-            case NotificationActions.ShowUpcomingBirthdays:
-              // Do other thing
-            default:
-              return;
-          }
-        }
-
-      })
-
-      FCM.getInitialNotification().then((payload) => {
-        // getInitialNotification is clicking the notification when the app is
-        // closed
-        this.onNotificationClick(payload);
-      })
-
-      this.refreshTokenListener = FCM.on('refreshToken', (token) => {
-        this.storeFCMToken(token);
-      })
-    }
-  }
-
-  componentWillUnmount() {
-    if(isAndroid){
-      this.notificationListener.remove();
-      this.refreshTokenListener.remove();
-    }
-  }
 
   render() {
     // Holding the store in state allows hot reloading
--- src/components/MainTabView.tsx
+++ src/components/MainTabView.tsx
@@ -6,7 +6,7 @@ import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-vi
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import {isIOS, WindowWidth} from '../Utilities';
+import {isAndroid, isIOS, WindowWidth} from '../Utilities';
 
 import {globalColors} from '../components/GlobalStyles';
 
@@ -15,6 +15,10 @@ import CContacts from '../containers/CContacts';
 import CBevegrams from '../containers/CBevegrams';
 import CBevegramLocations from '../containers/CBevegramLocations';
 import IconBadge from './IconBadge';
+
+import {NotificationActions} from '../api/notifications';
+import store from '../configureStore'
+import {sceneOrder, sceneKeys} from '../reducers/view';
 
 export interface MainViewRouterProps {
   currentPage?: string;
@@ -25,12 +29,94 @@ export interface MainViewRouterProps {
 }
 
 export default class MainViewRouter extends Component<MainViewRouterProps, {}> {
+  notificationListener: any;
+  refreshTokenListener: any;
 
   constructor(props){
     super(props);
     BackAndroid.addEventListener('hardwareBackPress', () => {
       this.props.goBackPage();
     });
+  }
+
+  storeFCMToken(token) {
+    store.dispatch({type: 'UPDATE_FCM_TOKEN', payload: {
+      fcmToken: token
+    }});
+  }
+
+  onNotificationClick(payload) {
+    switch(payload.action) {
+      case NotificationActions.ShowNewReceivedBevegrams:
+        this.showNewReceivedBevegrams();
+      case NotificationActions.ShowUpcomingBirthdays:
+        store.dispatch({type: 'SEND_BEVEGRAM_TO_CONTACT_VIA_NOTIFICATION', payload: {
+          facebookId: payload.facebookId,
+        }})
+      default:
+        return;
+    }
+  }
+
+  showNewReceivedBevegrams() {
+    store.dispatch({type: 'GO_TO_ROUTE', payload: {
+      route: "MainUi",
+    }});
+    store.dispatch({type: 'GOTO_VIEW', newScene: sceneOrder[sceneKeys.bevegrams]});
+  }
+
+  componentDidMount() {
+    if(isAndroid){
+      const FCM = require('react-native-fcm');
+      // FCM.requestPermissions();
+      // FCM.getFCMToken().then((token) => {
+      //   this.storeFCMToken(token);
+      // })
+
+      this.notificationListener = FCM.on('notification', (payload) => {
+        // There are 3 notification situations to handle here
+        // 1. App is in the foreground
+        // 2. App is in the background and notification clicked
+        // (payload.opened_from_tray is 1)
+        // 3. App is not running (caught in FCM.getInitialNotification)
+
+        if(payload.open_from_tray === 1){
+          this.onNotificationClick(payload);
+        } else {
+          switch(payload.action) {
+            case NotificationActions.ShowNewReceivedBevegrams:
+              store.dispatch({type: 'ADD_RECEIVED_BEVEGRAM_TO_BADGE', payload: {
+                // Should this badge show the number of bevegrams received or
+                // quantity: payload.quantity,
+                quantity: 1,
+              }})
+              store.dispatch({type: 'FETCH_RECEIVED_BEVEGRAMS'});
+            case NotificationActions.ShowUpcomingBirthdays:
+              // Do other thing
+            default:
+              return;
+          }
+        }
+
+      })
+
+      FCM.getInitialNotification().then((payload) => {
+        // getInitialNotification is clicking the notification when the app is
+        // closed
+        this.onNotificationClick(payload);
+      })
+
+      this.refreshTokenListener = FCM.on('refreshToken', (token) => {
+        this.storeFCMToken(token);
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    if(isAndroid){
+      this.notificationListener.remove();
+      this.refreshTokenListener.remove();
+    }
   }
 
   render() {
