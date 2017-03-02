50457fb28381165f2e6ee53789c43e711167573d
Andrew Simms <simms.andrew@gmail.com>
2017-03-02 13:46:08 -0700

    Test: Remove loading component tests from `Contacts`
    This component now uses `RefreshControl`.

--- __tests__/components/Contacts-test.js
+++ __tests__/components/Contacts-test.js
@@ -26,47 +26,5 @@ describe('Contacts component', () => {
     const list = wrapper.find(ListView);
     expect(list).toBeDefined();
   })
-
-  it('renders Loading view when loading', () => {
-    const wrapper = shallow(
-      <Contacts
-        contacts={[
-          {
-            name: { first: "Person", last: "McPerson" },
-            birthday: "12/12",
-            birthDayOfYear: 200,
-            imagePath: "test.jpg",
-          }
-        ]}
-        loading={true}
-        loadingFailed={false}
-        reloading={false}
-      />
-    )
-
-    const text = wrapper.find(View).find(Text).prop("children");
-    expect(text).toBe("Loading Contacts");
-  })
-
-  it('renders Failed loading view when loading failed', () => {
-    const wrapper = shallow(
-      <Contacts
-        contacts={[
-          {
-            name: { first: "Person", last: "McPerson" },
-            birthday: "12/12",
-            birthDayOfYear: 200,
-            imagePath: "test.jpg",
-          }
-        ]}
-        loading={false}
-        loadingFailed={true}
-        reloading={false}
-      />
-    )
-
-    const text = wrapper.find(View).find(Text).prop("children");
-    expect(text).toBe("Error loading Contacts, please log out and log in again.");
-  })
 })
 
