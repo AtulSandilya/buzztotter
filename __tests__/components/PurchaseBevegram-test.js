84130ac7893e699368e87b8ef623eeddbc8c4646
Andrew Simms <simms.andrew@gmail.com>
2017-03-02 13:46:39 -0700

    Test: Add `purchasePackages` to `PurchaseBevegram` test

--- __tests__/components/PurchaseBevegram-test.js
+++ __tests__/components/PurchaseBevegram-test.js
@@ -7,12 +7,19 @@ import renderer from 'react-test-renderer';
 import { initialPurchaseState } from '../../build/reducers/purchase';
 
 it('renders correctly', () => {
+  const purchasePackages = [{
+      name: "One",
+      quantity: 1,
+      price: 7.50,
+  }]
   const tree = renderer.create(
     <PurchaseBevegram
       fullName="Testy McTest"
       firstName="Testy"
       purchase={initialPurchaseState}
       creditCards={[]}
+      selectedPurchasePackage={purchasePackages[0]}
+      purchasePackages={purchasePackages}
     />
   ).toJSON();
   expect(tree).toMatchSnapshot();
