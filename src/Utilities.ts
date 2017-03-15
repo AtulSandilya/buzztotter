95ce39c90a95004e23c6ecaa39d10b5555e95ed0
Andrew Simms <simms.andrew@gmail.com>
2017-03-15 00:12:41 -0600

    Refactor: Use the Haversine Formula to calculate gps coordinate distances

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -21,11 +21,27 @@ export const StringifyDate = (): string => {
 }
 
 export const LocationsMatch = (a: DeviceLocation, b: DeviceLocation, name: string): boolean => {
-  const tolerance = 0.025;
-  const latitudeDiff = Math.abs(Math.abs(a.latitude) - Math.abs(b.latitude));
-  const longitudeDiff = Math.abs(Math.abs(a.longitude) - Math.abs(b.longitude));
+  // Meters
+  const tolerance = 50;
+  const metersBetweenPoints = HaversineFormula(a, b) * 1000;
 
-  return (latitudeDiff < tolerance) && (longitudeDiff < tolerance)
+  return tolerance > metersBetweenPoints;
+}
+
+// Returns the distance between two points in kilometers.
+// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+const HaversineFormula = (a: DeviceLocation, b: DeviceLocation) => {
+  const R = 6371; // Radius of the earth
+  const deg2rad = (deg) => deg * (Math.PI / 180);
+
+  const dLat = deg2rad(b.latitude - a.latitude);
+  const dLong = deg2rad(b.longitude - a.longitude);
+
+  const x = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(deg2rad(a.latitude)) * Math.cos(deg2rad(b.latitude)) *
+            Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  const z = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x));
+  return R * z;
 }
 
 export const Pluralize = (input: number, suffix: string = "s"): string => {
