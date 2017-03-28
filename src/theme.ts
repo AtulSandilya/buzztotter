d7c9a35189408879cfb709a30e5e6a0c78509e89
Andrew Simms <simms.andrew@gmail.com>
2017-03-28 15:33:10 -0600

    Theme: Add colors from global styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,17 @@
 const theme = {
+  /* tslint:disable:object-literal-sort-keys */
+  colors: {
+    // Tealish Green
+    bevPrimary: "#8ED0BA",
+    // Brown
+    bevSecondary: "#8B5E3C",
+    // Light Brown
+    bevActiveSecondary: "#AC9774",
+  },
   notificationIcons: {
-    cake: "ic_cake_white_48dp",
     beverage: "ic_local_bar_white_48dp",
-  }
-}
+    cake: "ic_cake_white_48dp",
+  },
+};
 
 export default theme;
