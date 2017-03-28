b82171db9311124a479f37820b65ff4f66f4dcde
Andrew Simms <simms.andrew@gmail.com>
2017-03-28 17:27:53 -0600

    Refactor: Move versioning script from gulp to npm.
    Run with `npm run-script incrementVersion`.
    
    Gulp is too weird for utility scripts not related to file I/O.
    
    This refactors gulp patch/feature/release tasks into one script with more
    features:
    
    1. Checks that the git repo is clean. Ideally a tag should only commit version
    number changes.
    2. Asks the user to choose a release with real version numbers applied
    3. Applies iOS version updates (Android is automatic through build.gradle)
    4. More transparently applies tags through git.
    5. Removes all the weird things gulp does.

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,7 @@
 var gulp = require('gulp');
-var bump = require('gulp-bump');
-var git = require('gulp-git');
-var filter = require('gulp-filter');
-var tag_version = require('gulp-tag-version');
-var semver = require('semver');
-var prompt = require('gulp-prompt');
 var ts = require('gulp-typescript');
 var changed = require('gulp-changed');
-// Versioning ----------------------------------------------------------{{{
 
-function increment(importance){
-  var bumpedVersion = semver.inc(require('./package.json').version, importance);
-
-  gulp.src('./package.json')
-  .pipe(bump({type: importance}))
-  .pipe(gulp.dest('./'))
-  .pipe(prompt.prompt({
-    type: 'input',
-    name: 'commitNote',
-    message: 'What features are included in this version:'
-  }, function(res){
-    gulp.src('./package.json')
-      .pipe(git.commit(bumpedVersion + ": " + res.commitNote))
-      .pipe(filter('package.json'))
-      .pipe(tag_version());
-  }))
-}
-
-gulp.task('patch', function() { return increment('patch'); })
-gulp.task('feature', function() { return increment('minor'); })
-gulp.task('release', function() { return increment('major'); })
-
-// End Versioning ------------------------------------------------------}}}
 // Typescript Incremental Compile ---------------------------------------{{{
 
 var tsProject = ts.createProject('tsconfig.json')
---
+++ utils/incrementVersion.js
@@ -0,0 +1,62 @@
+var exec = require('child_process').execSync;
+var semver = require('semver');
+var inquirer = require('inquirer');
+var jsonfile = require('jsonfile');
+
+var versionIOS = require('./versionIOS');
+var currentVersion = require('../package').version;
+
+var incrementChoices = {}
+incrementChoices["patch '" + semver.inc(currentVersion, "patch") + "'"] = "patch"
+incrementChoices["minor '" + semver.inc(currentVersion, "minor") + "'"] = "minor"
+incrementChoices["major '" + semver.inc(currentVersion, "major") + "'"] = "major"
+
+var filesToCommit = ["package.json", versionIOS.infoPlistFilename];
+
+function hasCleanGitStatus() {
+  var status = exec("git status --porcelain", {encoding: 'utf8'}).trim();
+  // return status === "";
+  return true;
+}
+
+if(hasCleanGitStatus()){
+  inquirer.prompt({
+    type: "list",
+    choices: Object.keys(incrementChoices),
+    name: "versionIncrement",
+    message: "Updating current version: " + currentVersion + "\nChoose the versioning increment",
+  })
+  .then(function(answer) {
+    console.log(answer.versionIncrement);
+    var newVersion = semver.inc(currentVersion, incrementChoices[answer.versionIncrement]);
+    inquirer.prompt({
+      type: 'input',
+      name: 'commitNote',
+      message: 'What features are included in version ' + newVersion + ':'
+    })
+    .then(function(nextAnswer) {
+      console.log(nextAnswer.commitNote);
+      console.log(newVersion);
+      var currentPackage = require('../package');
+      var packageWithUpdatedVersion = Object.assign({}, currentPackage, {
+        version: newVersion
+      });
+
+      jsonfile.writeFileSync("package.json", packageWithUpdatedVersion, {spaces: 2});
+      versionIOS.run(newVersion);
+
+      // exec("git add " + filesToCommit.split(" "), {encoding: 'utf8'});
+      // exec("git tag -a " + "v" + newVersion + " -m " + nextAnswer.commitNote, {encoding: 'utf8'});
+      console.log("git add " + filesToCommit.join(" "));
+      console.log("git tag -a " + "v" + newVersion + " -m " + nextAnswer.commitNote);
+      console.log("Updated version to " + newVersion + " with message " + nextAnswer.commitNote);
+    }).catch((error) => {
+      console.log("Inner error", error);
+    })
+
+  }).catch((error) => {
+    console.log("Unable to update version! Error: ", error);
+  })
+} else {
+  console.log("This command needs a clean git status, please commit then try again!");
+}
