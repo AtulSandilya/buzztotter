15b69629890616f0005314f9a8c0ff90cd319922
Andrew Simms <simms.andrew@gmail.com>
2017-04-02 22:48:58 -0600

    Lint

--- src/api/stripe.ts
+++ src/api/stripe.ts
@@ -1,22 +1,22 @@
-import moment from 'moment';
+import moment from "moment";
 
-import secrets from '../secrets';
+import secrets from "../secrets";
 
-const stripeUrl = 'https://api.stripe.com/v1/';
+const stripeUrl = "https://api.stripe.com/v1/";
 const apiKey = secrets.stripeApiKey;
 
 const uriEncodeObjectToString = (inputObj, separator = "&") => {
-  let result = [];
-  Object.keys(inputObj).forEach(function(key){
-    if(typeof inputObj[key] === 'object'){
+  const result = [];
+  Object.keys(inputObj).forEach((key) => {
+    if (typeof inputObj[key] === "object") {
       result.push(uriEncodeNestedObject(inputObj[key], key));
     } else {
       result.push(encodeURIComponent(key) + "=" + encodeURIComponent(inputObj[key]));
     }
-  })
+  });
   const strResult = result.join("&");
   return strResult;
-}
+};
 
 // Stripe does not accept json via the api. But they do accept key/value pairs
 // of an object. This function performs this conversion on an object. Example:
@@ -25,96 +25,96 @@ const uriEncodeObjectToString = (inputObj, separator = "&") => {
 // personStr = "person[first]=Test&person[last]=Name"
 
 const uriEncodeNestedObject = (inputObj, objName) => {
-  let result = [];
+  const result = [];
   const name = encodeURIComponent(objName);
   Object.keys(inputObj).forEach((key) => {
     result.push(`${name}[${encodeURIComponent(key)}]=${encodeURIComponent(inputObj[key])}`);
   });
-  return result.join("&")
-}
+  return result.join("&");
+};
 
 const stripeRequest = (
   url: string,
-  requestDetails: Object,
+  requestDetails: any,
   method: string = "POST",
 ) => {
   const body = uriEncodeObjectToString(requestDetails);
   return fetch(stripeUrl + url, {
-    method: method,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Bearer ' + secrets.stripeApiKey,
-    },
     body: uriEncodeObjectToString(requestDetails),
+    headers: {
+      "Accept": "application/json",
+      "Authorization": "Bearer " + secrets.stripeApiKey,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: method,
   })
   .then((response) => {
     return response.json();
   })
   .catch((error) => {
     throw Error(`Error ${method}ing ${body} to ${url}: ${error}`);
-  })
-}
+  });
+};
 
 // Verify a credit card. Returns a token if verification is successful.
 export const promiseCreditCardToken = (cardNumber, expMonth, expYear, cvc) => {
   const cardDetails = {
     card: {
-      number: cardNumber,
+      cvc: cvc,
       exp_month: expMonth,
       exp_year: expYear,
-      cvc: cvc,
-    }
+      number: cardNumber,
+    },
   };
 
   return stripeRequest("tokens", cardDetails);
-}
+};
 
 export const promiseCreditCardPurchase = (customerId, amount, description) => {
   const purchaseDetails = {
-    "amount": amount,
-    "currency": "usd",
-    "customer": customerId,
-    "description": description,
-  }
+    amount: amount,
+    currency: "usd",
+    customer: customerId,
+    description: description,
+  };
 
   return stripeRequest("charges", purchaseDetails);
-}
+};
 
 // Create a new empty stripe customer
 export const promiseNewCustomer = (fullName: string, email: string) => {
   const newCustomerDetails = {
     description: `${fullName}: ${email}`,
     metadata: {
-      name: fullName,
-      email: email,
       creationDate: moment().format("MM/DD/YYYY HH:MM:ss Z"),
-    }
-  }
+      email: email,
+      name: fullName,
+    },
+  };
 
   return stripeRequest("customers", newCustomerDetails);
-}
+};
 
 export const promiseAddCardToCustomer = (customerId: string, token: string) => {
   const customerDetails = {
     source: token,
-  }
+  };
 
   return stripeRequest("customers/" + customerId + "/sources", customerDetails);
-}
+};
 
 export const promiseUpdateCustomerDefaultCard = (customerId: string, newDefaultCard: string) => {
   const update = {
-    "default_source": newDefaultCard,
-  }
+    default_source: newDefaultCard,
+  };
 
   return stripeRequest("customers/" + customerId, update);
-}
+};
 
 export const promiseCustomer = (customerId: string) => {
   return stripeRequest("customers/" + customerId, {});
-}
+};
 
 export const promiseDeleteCustomerCard = (customerId: string, cardToDelete: string) => {
-  return stripeRequest("customers/" + customerId + "/sources/" + cardToDelete, {}, 'DELETE');
-}
+  return stripeRequest("customers/" + customerId + "/sources/" + cardToDelete, {}, "DELETE");
+};
