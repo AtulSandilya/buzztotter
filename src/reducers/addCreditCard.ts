41de2a716bb20b3ad146a6113a20e9d8031bf02d
Andrew Simms <simms.andrew@gmail.com>
2017-04-10 20:19:15 -0600

    Lint

--- src/reducers/addCreditCard.ts
+++ src/reducers/addCreditCard.ts
@@ -14,45 +14,45 @@ interface AddCreditCardState {
 
 const initialAddCreditCardState: AddCreditCardState = {
   attemptingVerification: false,
-  isVerified: false,
-  failed: false,
   failMessage: "",
-}
+  failed: false,
+  isVerified: false,
+};
 
 export const addCreditCard = (state = initialAddCreditCardState, action) => {
-  switch(action.type){
-    case 'ATTEMPTING_CREDIT_CARD_VERIFICATION':
+  switch (action.type) {
+    case "ATTEMPTING_CREDIT_CARD_VERIFICATION":
       return Object.assign({}, state, {
           attemptingVerification: true,
         });
-    case 'SUCCESSFUL_CREDIT_CARD_VERIFICATION':
+    case "SUCCESSFUL_CREDIT_CARD_VERIFICATION":
       return Object.assign({}, state, {
           attemptingVerification: false,
           isVerified: true,
         });
-    case 'FAILED_STRIPE_ADD_CARD_TO_CUSTOMER':
-    case 'FAILED_CREDIT_CARD_VERIFICATION':
+    case "FAILED_STRIPE_ADD_CARD_TO_CUSTOMER":
+    case "FAILED_CREDIT_CARD_VERIFICATION":
       return Object.assign({}, state, {
           attemptingVerification: false,
-          failed: true,
           failMessage: action.payload.error,
+          failed: true,
         });
-    case 'THROWN_STRIPE_ADD_CARD_TO_CUSTOMER':
-    case 'THROWN_CREDIT_CARD_VERIFICATION':
+    case "THROWN_STRIPE_ADD_CARD_TO_CUSTOMER":
+    case "THROWN_CREDIT_CARD_VERIFICATION":
       return Object.assign({}, state, {
           attemptingVerification: false,
-          failed: true,
           failMessage: state.failMessage.length > 0 ? state.failMessage : action.payload.error,
+          failed: true,
         });
-    case 'END_CREDIT_CARD_VERIFICATION_IF_NOT_ATTEMPTING':
-      if(state.attemptingVerification){
+    case "END_CREDIT_CARD_VERIFICATION_IF_NOT_ATTEMPTING":
+      if (state.attemptingVerification) {
         return state;
       } else {
         return initialAddCreditCardState;
       }
-    case 'END_CREDIT_CARD_VERIFICATION':
+    case "END_CREDIT_CARD_VERIFICATION":
       return initialAddCreditCardState;
     default:
       return state;
   }
-}
+};
