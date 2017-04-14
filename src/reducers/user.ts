c9c0f68f3a53df9fda2e0996179d51ca93422350
Andrew Simms <simms.andrew@gmail.com>
2017-04-14 11:55:20 -0600

    Lint

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -8,47 +8,47 @@ const mapFacebookDataToState = (state, action): User => {
   const data = action.payload.userData;
   return Object.assign({}, state,
     {
-      isLoggedIn: true,
-      facebook: {
-        token: action.payload.token,
-        id: action.payload.userData.id,
-      },
-      firstName: data.first_name,
-      lastName: data.last_name,
-      fullName: data.name,
       birthday: data.birthday,
       email: data.email,
-    }
+      facebook: {
+        id: action.payload.userData.id,
+        token: action.payload.token,
+      },
+      firstName: data.first_name,
+      fullName: data.name,
+      isLoggedIn: true,
+      lastName: data.last_name,
+    },
   );
-}
+};
 
 export const user = (state = defaultState, action): User => {
-  switch(action.type){
-    case('POPULATE_USER_DATA_FROM_FACEBOOK'):
+  switch (action.type) {
+    case("POPULATE_USER_DATA_FROM_FACEBOOK"):
       return mapFacebookDataToState(state, action);
-    case 'LOGIN_FACEBOOK':
+    case "LOGIN_FACEBOOK":
       return Object.assign({}, state, {
         isLoggedIn: true,
       });
-    case 'LOGOUT_FACEBOOK':
+    case "LOGOUT_FACEBOOK":
       return Object.assign({}, state, {
         isLoggedIn: false,
       });
-    case 'SUCCESSFUL_FIREBASE_LOGIN':
+    case "SUCCESSFUL_FIREBASE_LOGIN":
       return Object.assign({}, state, {
         firebase: action.payload.firebaseUser,
-      })
-    case 'STORE_FCM_TOKEN':
+      });
+    case "STORE_FCM_TOKEN":
       return Object.assign({}, state, {
         fcmToken: action.payload.fcmToken,
-    })
-    case 'UPDATE_LAST_MODIFIED':
+    });
+    case "UPDATE_LAST_MODIFIED":
       return Object.assign({}, state, {
         lastModified: action.payload.lastModified,
-    })
+    });
     case "REWRITE_USER":
       return Object.assign({}, action.payload.updatedUser);
     default:
       return state;
   }
-}
+};
