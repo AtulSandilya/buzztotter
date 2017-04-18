00ffac36dff5f01f574321510150365ac26dd784
Andrew Simms <simms.andrew@gmail.com>
2017-04-18 00:34:17 -0600

    Server: Add "Add Card to Customer" Queue actions
    This is the first major part moving client functionality onto the server.
    
    From a high level this takes the `addCreditCardToCustomer` saga and does the
    same thing. The main difference is we don't have to check for an internet
    connection after each action, we can basically assume that there will be no
    connection errors.
    
    This uses relatively new javascript `async` and `await` functionality for
    cleaner code. This requires configuring babel to transpile for the current
    node version.
    
    `verifyUser` is a hack to ensure the sender is who the say they are.
    `firebase-queue` does not provide a mechanism to identify the sender, so
    before the queue is written to the client write a token to the
    `verificationToken` node. The client can only write to their node, so if the
    verification token sent does not match the verification token in the database
    the client is not who they say they are and the process terminates.
    
    `updateUser` is a helper function to ensure `user.lastModified` gets updated
    to trigger the `child_changed` listener on the client. Failure to do this
    results in a broken client state.

--- src/firebaseServer/index.ts
+++ src/firebaseServer/index.ts
@@ -1,14 +1,86 @@
 import Queue from "firebase-queue";
 
-import {GetNotificationQueueUrl} from "../db/schema";
+import * as DbSchema from "../db/schema";
+
+import {
+  AddCreditCardToCustomerPackageForQueue,
+  PurchasePackageForQueue,
+  RedeemPackageForQueue,
+  RemoveCreditCardFromCustomerPackageForQueue,
+  STRIPE_MAX_NUMBER_OF_CREDIT_CARDS,
+  StripeCreditCard,
+  UpdateDefaultCreditCardForCustomerPackageForQueue,
+  User,
+} from "../db/tables";
+
+import * as stripe from "./stripe";
+
+import FirebaseDb from "../api/firebase/FirebaseDb";
+
+import Log from "./Log";
+
 import {sendNotification} from "./notifications";
 import {SetupAdminDb} from "./utils";
 
-const db = SetupAdminDb();
+const db = new FirebaseDb(SetupAdminDb());
 
 /* tslint:disable:no-console */
-console.log("Starting Firebase Server...");
-console.log("Listening for changes on node: ", GetNotificationQueueUrl());
+const AddCardToCustomerQueueUrl = DbSchema.GetAddCreditCardToCustomerQueueUrl();
+Log.StartQueueMessage(AddCardToCustomerQueueUrl);
+const AddCardToCustomerQueue = new Queue(db.getRef(AddCardToCustomerQueueUrl),
+  (data, progress, resolve, reject) => {
+    const log = new Log("ADD_CARD_TO_CUSTOMER");
+    const process = async () => {
+      const input: AddCreditCardToCustomerPackageForQueue = data;
+      const {stripeCreditCardToken, userFirebaseId, verificationToken} = input;
+      const user: User = await db.readNode(DbSchema.GetUserDbUrl(userFirebaseId));
+      try {
+        // 1. Check user in firebase to see if there is a stripe customer id
+        // 2. If no stripe customer create stripe customer
+        // 3. Add card to customer on stripe
+        // 4. Add card id to firebase
+        await verifyUser(verificationToken, userFirebaseId);
+
+        // TODO: Match the stripe token against some regex
+
+        let userStripeId = await db.readNode(DbSchema.GetStripeCustomerIdDbUrl(userFirebaseId));
+
+        if (!userStripeId) {
+          userStripeId = (await stripe.promiseNewCustomer(user.fullName, user.email)).id;
+          await db.writeNode(DbSchema.GetStripeCustomerIdDbUrl(userFirebaseId), userStripeId);
+        } else {
+          if (!user.stripe) {
+            user.stripe = await stripe.promiseCustomer(userStripeId);
+          }
+
+          if (user.stripe && user.stripe.creditCards.length >= STRIPE_MAX_NUMBER_OF_CREDIT_CARDS) {
+            throw new QueueServerError(`Cannot add more than ${STRIPE_MAX_NUMBER_OF_CREDIT_CARDS} credit cards`);
+          }
+        }
+
+        const addedCard = await stripe.promiseAddCardToCustomer(userStripeId, stripeCreditCardToken);
+        await stripe.promiseUpdateCustomerDefaultCard(userStripeId, addedCard.metadata.generatedId);
+
+        user.stripe = await stripe.promiseCustomer(userStripeId);
+
+        await updateUser(input.userFirebaseId, user);
+
+        log.successMessage();
+        resolve();
+      } catch (e) {
+        user.stripe = Object.assign({}, user.stripe, {
+          error: e,
+        });
+
+        await updateUser(input.userFirebaseId, user);
+
+        log.failMessage(e);
+
+        resolve();
+      }
+    };
+    process();
+});
 
 const notificationQueue = new Queue(db.ref(GetNotificationQueueUrl()), (data, progress, resolve, reject) => {
   console.log("New Queue Request, data:", data);
@@ -16,3 +88,29 @@ const notificationQueue = new Queue(db.ref(GetNotificationQueueUrl()), (data, pr
   console.log("Completed Queue Request");
   resolve();
 });
+
+const verifyUser = async (verificationToken: string, userFirebaseId: string) => {
+  const tokenInDb = await db.readNode(DbSchema.GetUserVerificationTokenDbUrl(userFirebaseId));
+
+  if (tokenInDb !== verificationToken) {
+    console.log(`${tokenInDb} != ${verificationToken}`);
+    throw new QueueServerError("Verification tokens do not match!");
+  }
+};
+
+const updateUser = async (userFirebaseId, user: User) => {
+  // This is important as it triggers a "child_changed" event which the
+  // client is listening for.
+  const updatedUser = Object.assign({}, user, {
+    lastModified: Date.now().toString(),
+  });
+  await db.writeNode(DbSchema.GetUserDbUrl(userFirebaseId), updatedUser);
+};
+
+class QueueServerError extends Error {
+  constructor(message) {
+    super(message);
+    this.message = message;
+    this.name = "QueueServerError";
+  }
+}
