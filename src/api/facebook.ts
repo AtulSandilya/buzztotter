6f0f5cf0ccb7fb09128dd612cd77bdeee942120e
Andrew Simms <simms.andrew@gmail.com>
2017-04-21 22:13:50 -0600

    Lint

--- src/api/facebook.ts
+++ src/api/facebook.ts
@@ -1,31 +1,32 @@
-import { GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+import { GraphRequest, GraphRequestManager } from "react-native-fbsdk";
 
 const graphRequest = (token, urlString, parameterString, callback) => {
   // If parameterString is empty the the parameters object needs to be an
   // empty object else the graph api returns an error
-  let requestParams = {}
-  if(parameterString){
-    requestParams["fields"] = {
+  const requestParams = {};
+  if (parameterString) {
+    const fieldParam = "fields";
+    requestParams[fieldParam] = {
       string: parameterString,
-    }
+    };
   }
 
   return new GraphRequest(
     urlString,
     {
-      httpMethod: 'GET',
-      version: 'v2.7',
-      parameters: requestParams,
       accessToken: token.toString(),
+      httpMethod: "GET",
+      parameters: requestParams,
+      version: "v2.7",
     },
     callback,
   );
-}
+};
 
 const promiseFunctionFromFacebook = (token, inputFunction) => {
-  var promise = new Promise(function(resolve, reject){
-    new GraphRequestManager().addRequest(inputFunction(token, function(error, result){
-      if(error){
+  const promise = new Promise((resolve, reject) => {
+    new GraphRequestManager().addRequest(inputFunction(token, (error, result) => {
+      if (error) {
         reject(error);
       } else {
         resolve(result);
@@ -33,24 +34,24 @@ const promiseFunctionFromFacebook = (token, inputFunction) => {
     })).start();
   });
   return promise;
-}
+};
 
 const getFriendsRequest = (token, callback) => {
-  const urlString = '/me/friends?fields=picture.type(square),first_name,last_name,name,birthday,email&limit=5000';
+  const urlString = "/me/friends?fields=picture.type(square),first_name,last_name,name,birthday,email&limit=5000";
   const parameterString = "";
   return graphRequest(token, urlString, parameterString, callback);
-}
+};
 
 export const promiseContactsFromFacebook = (token) => {
   return promiseFunctionFromFacebook(token, getFriendsRequest);
-}
+};
 
 const userInfoRequest = (token, callback) => {
   const urlString = "/me";
   const parameterString = "id, name, first_name, last_name, email, birthday";
   return graphRequest(token, urlString, parameterString, callback);
-}
+};
 
 export const promiseUserInfoFromFacebook = (token) => {
   return promiseFunctionFromFacebook(token, userInfoRequest);
-}
+};
