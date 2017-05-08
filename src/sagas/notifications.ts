15c286fc58959c0b6b000f9c460dde51cf799511
Andrew Simms <simms.andrew@gmail.com>
2017-05-07 22:56:37 -0600

    Refactor: Remove notification sending code from the client

--- src/sagas/notifications.ts
+++ src/sagas/notifications.ts
@@ -1,42 +1,11 @@
 import {call, put, select} from "redux-saga/effects";
 
+import {User} from "../db/tables";
+
 import {
-  addNotificationToNotificationQueue,
-  getFcmToken,
-  isUserLoggedIn,
   setFcmToken,
   updateFirebaseUser,
 } from "../api/firebase";
-
-import theme from "../theme";
-
-import {
-  Notification,
-  NotificationActions,
-} from "../db/tables";
-import {User} from "../db/tables";
-
-export function *sendReceivedNotification(action, receiverFacebookId: string) {
-  const receiverFcmToken: string = yield call(getFcmToken, receiverFacebookId);
-
-  const userState: UserState = yield select<{user: UserState}>((state) => state.user);
-  const sender = userState.fullName;
-  const quantity: number = action.payload.sendBevegramData.quantity;
-  const bevStr = "bevegram" + (quantity !== 1 ? "s" : "");
-
-  const notification: Notification = {
-    action: NotificationActions.ShowNewReceivedBevegrams,
-    body: `${sender} sent you ${quantity} ${bevStr}`,
-    // Shorthand syntax, can be accessed with data.quantity
-    data: { quantity },
-    icon: theme.notificationIcons.beverage,
-    receiverGCMId: receiverFcmToken,
-    title: `You received ${quantity} ${bevStr}`,
-  };
-
-  yield call(addNotificationToNotificationQueue, notification);
-
-}
 
 export function *storeFcmToken(action) {
   yield put({type: "STORE_FCM_TOKEN", payload: {
@@ -45,7 +14,7 @@ export function *storeFcmToken(action) {
 }
 
 export function *dbWriteFcmToken() {
-  const userState: UserState = yield select<{user: UserState}>((state) => state.user);
+  const userState: User = yield select<{user: User}>((state) => state.user);
   yield call(setFcmToken, userState.facebook.id, userState.fcmToken);
   yield call(updateFirebaseUser, userState);
 }
