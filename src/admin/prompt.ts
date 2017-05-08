9097fd066c24f2ecb1c23aad0832e475ecfe1e87
Andrew Simms <simms.andrew@gmail.com>
2017-05-08 16:08:13 -0600

    Admin: Add framework for requesting basic types on the command line

---
+++ src/admin/prompt.ts
@@ -0,0 +1,62 @@
+import * as inquirer from "inquirer";
+
+export const getString = async (message): Promise<string> => {
+  const result = await inquirer.prompt({
+    message: `${message}:`,
+    name: "answer",
+    type: "input",
+  });
+  return result.answer;
+};
+
+export const getInteger = async (message): Promise<number> => {
+  const result = await inquirer.prompt({
+    message: `${message}:`,
+    name: "answer",
+    type: "input",
+    validate: (input) => {
+      if (isNaN(input as any)) {
+        return `'${message}' requires a number for the answer!`;
+      } else {
+        return true;
+      }
+    },
+  });
+  const base10 = 10;
+  return parseInt(result.answer, base10);
+};
+
+export const confirm = async (message: string) => {
+  const result = await inquirer.prompt({
+    message: `${message}?`,
+    name: "answer",
+    type: "confirm",
+  });
+  return result.answer;
+};
+
+export const getChoice = async (message: string, choices: string[]): Promise<string> => {
+  const result = await inquirer.prompt({
+    message: `${message}?`,
+    name: "answer",
+    type: "list",
+    choices,
+  });
+  return result.answer;
+};
+
+const untilCorrect = async (promptFunc: () => any, isCorrectMessage?: string) => {
+  while (true) {
+    const answer = await promptFunc();
+    const shouldExit = await confirm(isCorrectMessage ? isCorrectMessage : `Is "${answer}" correct`);
+    if (shouldExit) {
+      return answer;
+    }
+  }
+};
+
+export const getStringUntilCorrect = async (message: string, isCorrectMessage?: string) => {
+  return await untilCorrect(async () => {
+    return await getString(message);
+  }, isCorrectMessage);
+};
