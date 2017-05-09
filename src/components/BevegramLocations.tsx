8da7e4cdf28851c60a63b3e551641a3ec61c99f4
Andrew Simms <simms.andrew@gmail.com>
2017-05-09 17:00:04 -0600

    Ui: Put bar reload icon and text on one line

--- src/components/BevegramLocations.tsx
+++ src/components/BevegramLocations.tsx
@@ -127,7 +127,7 @@ export default class BevegramLocations extends Component<BevegramLocationsProps,
                   margin: 10,
                   marginBottom: 0,
                 }}>
-                  <View style={{flex: 1}}>
+                  <View style={{flex: 1, flexDirection: "row", alignItems: "center"}}>
                     <Text style={globalStyles.titleText}>Buzz Otter Bars:</Text>
                     {this.props.isReloading ?
                       <ActivityIndicator />
