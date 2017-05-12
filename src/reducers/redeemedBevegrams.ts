df1af72188553fbda1e2f2625d3e6697822c1777
Andrew Simms <simms.andrew@gmail.com>
2017-05-12 17:15:48 -0600

    Lint

--- src/reducers/redeemedBevegrams.ts
+++ src/reducers/redeemedBevegrams.ts
@@ -1,15 +1,15 @@
 const defaultState = {};
 
 export const redeemedBevegrams = (state = defaultState, action) => {
-  switch(action.type) {
-    case 'ADD_REDEEMED_BEVEGRAMS': {
+  switch (action.type) {
+    case "ADD_REDEEMED_BEVEGRAMS": {
       const newState = Object.assign({}, state);
       newState[action.payload.redeemedBevegramPack.id] = action.payload.redeemedBevegramPack.redeemedBevegram;
       return newState;
     }
-    case 'SET_REDEEMED_BEVEGRAM_LIST':
+    case "SET_REDEEMED_BEVEGRAM_LIST":
       return Object.assign({}, action.payload.list);
     default:
       return state;
   }
-}
+};
