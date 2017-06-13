d950fa2bada8ff7192f9e7845b5de456d5a404b6
Andrew Simms <simms.andrew@gmail.com>
2017-06-12 23:20:58 -0600

    Server: Remove `TEST` prefix from env variables
    Environment variables are will be deployed to heroku based on their
    prefix in the `.env` file. This means the build environment is not based
    on a global environment variable, just the actual environment variables.

--- src/firebaseServer/SetupAdminDb.ts
+++ src/firebaseServer/SetupAdminDb.ts
@@ -3,21 +3,21 @@ import * as admin from "firebase-admin";
 const SetupAdminDb = () => {
   require("dotenv").config();
   const serviceAccount = {
-    auth_provider_x509_cert_url: process.env.TEST_FIREBASE_ADMIN_KEY_auth_provider_x509_cert_url,
-    auth_uri: process.env.TEST_FIREBASE_ADMIN_KEY_auth_uri,
-    client_email: process.env.TEST_FIREBASE_ADMIN_KEY_client_email,
-    client_id: process.env.TEST_FIREBASE_ADMIN_KEY_client_id,
-    client_x509_cert_url: process.env.TEST_FIREBASE_ADMIN_KEY_client_x509_cert_url,
-    private_key: process.env.TEST_FIREBASE_ADMIN_KEY_private_key.replace(/\\n/g, "\n"),
-    private_key_id: process.env.TEST_FIREBASE_ADMIN_KEY_private_key_id,
-    project_id: process.env.TEST_FIREBASE_ADMIN_KEY_project_id,
-    token_uri: process.env.TEST_FIREBASE_ADMIN_KEY_token_uri,
-    type: process.env.TEST_FIREBASE_ADMIN_KEY_type,
+    auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_KEY_auth_provider_x509_cert_url,
+    auth_uri: process.env.FIREBASE_ADMIN_KEY_auth_uri,
+    client_email: process.env.FIREBASE_ADMIN_KEY_client_email,
+    client_id: process.env.FIREBASE_ADMIN_KEY_client_id,
+    client_x509_cert_url: process.env.FIREBASE_ADMIN_KEY_client_x509_cert_url,
+    private_key: process.env.FIREBASE_ADMIN_KEY_private_key.replace(/\\n/g, "\n"),
+    private_key_id: process.env.FIREBASE_ADMIN_KEY_private_key_id,
+    project_id: process.env.FIREBASE_ADMIN_KEY_project_id,
+    token_uri: process.env.FIREBASE_ADMIN_KEY_token_uri,
+    type: process.env.FIREBASE_ADMIN_KEY_type,
   };
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.TEST_FIREBASE_KEY_firebaseDatabaseURL,
+    databaseURL: process.env.FIREBASE_KEY_firebaseDatabaseURL,
   });
 
   return admin.database();
