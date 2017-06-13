43c98f4777f5055703aa5267fe116d98d2b6fabe
Andrew Simms <simms.andrew@gmail.com>
2017-06-13 10:42:40 -0600

    Admin: Add colored logging commands

---
+++ src/admin/log.ts
@@ -0,0 +1,6 @@
+import * as colors from "colors";
+
+/* tslint:disable:no-console */
+export const message = (input: string) => console.log(colors.blue.bold(input));
+export const success = (input: string) => console.log(colors.green.bold(input));
+export const failure = (input: string) => console.log("\n" + colors.red.bold(input));
