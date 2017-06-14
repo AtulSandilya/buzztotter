d8d62d080601de3332d58cef1bd938188b89f3f0
Andrew Simms <simms.andrew@gmail.com>
2017-06-14 14:37:10 -0600

    Lint

--- src/components/BevegramLocations.tsx
+++ src/components/BevegramLocations.tsx
@@ -13,9 +13,9 @@ import {
 
 import MapView from "react-native-maps";
 
-import {isAndroid, isIOS} from "../ReactNativeUtilities";
+import { isAndroid, isIOS } from "../ReactNativeUtilities";
 
-import {Location} from "../db/tables";
+import { Location } from "../db/tables";
 
 import BevButton from "./BevButton";
 import BevUiText from "./BevUiText";
@@ -23,18 +23,22 @@ import TitleText from "./TitleText";
 
 import theme from "../theme";
 
-import {globalColors, globalStyles} from "./GlobalStyles";
+import { globalColors, globalStyles } from "./GlobalStyles";
 
 const openMapsToAddress = (latitude, longitude, name) => {
   let url;
   // encodeURIComponent properly converts characters into url format.
   if (isAndroid) {
-    url = `geo:${latitude},${longitude}?q=${latitude},${longitude}(${encodeURIComponent(name)})`;
+    url = `geo:${latitude},${longitude}?q=${latitude},${longitude}(${encodeURIComponent(
+      name,
+    )})`;
   } else if (isIOS) {
-    url = `http://maps.apple.com/?ll=${latitude},${longitude}&q=${encodeURIComponent(name)}`;
+    url = `http://maps.apple.com/?ll=${latitude},${longitude}&q=${encodeURIComponent(
+      name,
+    )}`;
   }
 
-  Linking.canOpenURL(url).then((supported) => {
+  Linking.canOpenURL(url).then(supported => {
     if (supported) {
       Linking.openURL(url);
     } else {
@@ -51,9 +55,14 @@ export interface BevegramLocationsProps {
   getNearestLocations?(): void;
 }
 
-export default class BevegramLocations extends Component<BevegramLocationsProps, {}> {
+export default class BevegramLocations extends Component<
+  BevegramLocationsProps,
+  {}
+> {
   render() {
-    const locationDS = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const locationDS = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
     // `react-native-maps` renders the markers in order meaning the last
     // marker is above the first marker. Reversing the markers renders the
     // first location on top
@@ -65,81 +74,107 @@ export default class BevegramLocations extends Component<BevegramLocationsProps,
 
     const todayAsNumber = new Date().getDay();
 
-    return(
-      <View style={{flex: 1}}>
+    return (
+      <View style={{ flex: 1 }}>
         <MapView
-            style={{flex: 6}}
-            initialRegion={{
-              latitude: 39.7392358,
-              longitude: -104.990251,
-              latitudeDelta: 0.0922 * 1.75,
-              longitudeDelta: 0.0421 * 1.75,
-            }}
-          >
+          style={{ flex: 6 }}
+          initialRegion={{
+            latitude: 39.7392358,
+            longitude: -104.990251,
+            latitudeDelta: 0.0922 * 1.75,
+            longitudeDelta: 0.0421 * 1.75,
+          }}
+        >
           {reversedMarkers.map((markerData, index) => {
             const numMarkers = this.props.markers.length;
             return (
               <MapView.Marker
                 key={index}
-                style={{top: -15}}
+                style={{ top: -15 }}
                 coordinate={{
                   latitude: markerData.latitude,
                   longitude: markerData.longitude,
                 }}
               >
-              <View style={{flex: -1, alignItems: "center", maxWidth: 185}}>
-                <View style={{flex: -1, padding: 6, backgroundColor: globalColors.bevPrimary, borderRadius: 3, flexDirection: "row", borderWidth: 1, borderColor: "#000000"}}>
-                  <View style={{paddingLeft: 10, flex: -1, alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-                    <Text style={{fontSize: 18, fontWeight: "bold"}}>{numMarkers - index}. {markerData.name}</Text>
-                    <BevUiText icon="calendar" color="#000000">
-                      {markerData.typicalHours[todayAsNumber].split(": ").slice(-1)[0]}
-                    </BevUiText>
+                <View style={{ flex: -1, alignItems: "center", maxWidth: 185 }}>
+                  <View
+                    style={{
+                      flex: -1,
+                      padding: 6,
+                      backgroundColor: globalColors.bevPrimary,
+                      borderRadius: 3,
+                      flexDirection: "row",
+                      borderWidth: 1,
+                      borderColor: "#000000",
+                    }}
+                  >
+                    <View
+                      style={{
+                        paddingLeft: 10,
+                        flex: -1,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                        {numMarkers - index}. {markerData.name}
+                      </Text>
+                      <BevUiText icon="calendar" color="#000000">
+                        {
+                          markerData.typicalHours[todayAsNumber]
+                            .split(": ")
+                            .slice(-1)[0]
+                        }
+                      </BevUiText>
+                    </View>
                   </View>
+                  <View
+                    style={{
+                      flex: -1,
+                      alignSelf: "center",
+                      width: 0,
+                      height: 0,
+                      backgroundColor: "transparent",
+                      borderStyle: "solid",
+                      borderLeftWidth: 10,
+                      borderRightWidth: 10,
+                      borderTopWidth: 15,
+                      borderLeftColor: "transparent",
+                      borderRightColor: "transparent",
+                      borderTopColor: globalColors.bevPrimary,
+                      zIndex: 10,
+                      top: -1,
+                    }}
+                  />
+                  <View
+                    style={{
+                      position: "relative",
+                      top: -15,
+                      flex: -1,
+                      alignSelf: "center",
+                      width: 0,
+                      height: 0,
+                      backgroundColor: "transparent",
+                      borderStyle: "solid",
+                      borderLeftWidth: 11,
+                      borderRightWidth: 11,
+                      borderTopWidth: 16,
+                      borderLeftColor: "transparent",
+                      borderRightColor: "transparent",
+                      borderTopColor: "#000000",
+                      zIndex: 5,
+                    }}
+                  />
                 </View>
-                <View style={{
-                  flex: -1,
-                  alignSelf: "center",
-                  width: 0,
-                  height: 0,
-                  backgroundColor: "transparent",
-                  borderStyle: "solid",
-                  borderLeftWidth: 10,
-                  borderRightWidth: 10,
-                  borderTopWidth: 15,
-                  borderLeftColor: "transparent",
-                  borderRightColor: "transparent",
-                  borderTopColor: globalColors.bevPrimary,
-                  zIndex: 10,
-                  top: -1,
-                }}>
-                </View>
-                <View style={{
-                  position: "relative",
-                  top: -15,
-                  flex: -1,
-                  alignSelf: "center",
-                  width: 0,
-                  height: 0,
-                  backgroundColor: "transparent",
-                  borderStyle: "solid",
-                  borderLeftWidth: 11,
-                  borderRightWidth: 11,
-                  borderTopWidth: 16,
-                  borderLeftColor: "transparent",
-                  borderRightColor: "transparent",
-                  borderTopColor: "#000000",
-                  zIndex: 5,
-                }}>
-                </View>
-              </View>
-            </MapView.Marker>
+              </MapView.Marker>
             );
           })}
         </MapView>
-        <View style={{flex: 4}}>
+        <View style={{ flex: 4 }}>
           <ListView
             dataSource={locationDS.cloneWithRows(this.props.markers)}
-            renderHeader={() => (
+            renderHeader={() =>
               <TouchableHighlight
                 underlayColor={"#ffffff"}
                 onPress={() => {
@@ -151,15 +186,37 @@ export default class BevegramLocations extends Component<BevegramLocationsProps,
                   flex: 1,
                   margin: 6,
                   marginBottom: 0,
-                }}>
-                  <View style={{flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-                    <View style={{flex: -1}}>
-                      <Text style={[globalStyles.titleText, {flex: -1, alignSelf: "flex-start"}]}>Buzz Otter Bars</Text>
-                    </View>
-                    <View style={{flex: 1, alignItems: "flex-end", justifyContent: "center", paddingRight: 8}}>
-                      {this.props.isReloading ?
-                        <View style={{flexDirection: "row"}}>
-                          <ActivityIndicator style={{paddingRight: 8}}/>
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <View style={{ flex: -1 }}>
+                    <Text
+                      style={[
+                        globalStyles.titleText,
+                        { flex: -1, alignSelf: "flex-start" },
+                      ]}
+                    >
+                      Buzz Otter Bars
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: "flex-end",
+                      justifyContent: "center",
+                      paddingRight: 8,
+                    }}
+                  >
+                    {this.props.isReloading
+                      ? <View style={{ flexDirection: "row" }}>
+                          <ActivityIndicator style={{ paddingRight: 8 }} />
                           <BevUiText>
                             Refreshing
                           </BevUiText>
@@ -167,22 +224,28 @@ export default class BevegramLocations extends Component<BevegramLocationsProps,
                       : <BevUiText icon="refresh" iconSize="large">
                           Refresh
                         </BevUiText>}
-                    </View>
                   </View>
-              </TouchableHighlight>
-            )}
-            renderRow={(rowData, sectionId, rowId) =>
-              <View style={{flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "center", padding: 10}}>
-                <View style={{flex: 1, flexDirection: "column"}}>
-                  <Text style={{fontSize: 20, fontWeight: "bold"}}>{parseInt(rowId as any, 10) + 1}. {rowData.name}</Text>
-                    <BevUiText
-                      icon="calendar"
-                      style={{paddingTop: 5}}
-                    >
-                      {rowData.typicalHours[todayAsNumber]}
-                    </BevUiText>
                 </View>
-                <View style={{flex: 1}}>
+              </TouchableHighlight>}
+            renderRow={(rowData, sectionId, rowId) =>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: 10,
+                }}
+              >
+                <View style={{ flex: 1, flexDirection: "column" }}>
+                  <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                    {parseInt(rowId as any, 10) + 1}. {rowData.name}
+                  </Text>
+                  <BevUiText icon="calendar" style={{ paddingTop: 5 }}>
+                    {rowData.typicalHours[todayAsNumber]}
+                  </BevUiText>
+                </View>
+                <View style={{ flex: 1 }}>
                   <BevButton
                     text={"Open in Maps"}
                     shortText="Map"
@@ -190,32 +253,32 @@ export default class BevegramLocations extends Component<BevegramLocationsProps,
                     buttonFontSize={16}
                     margin={5}
                     rightIcon={true}
-                    onPress={() => openMapsToAddress(rowData.latitude, rowData.longitude, rowData.name)}
+                    onPress={() =>
+                      openMapsToAddress(
+                        rowData.latitude,
+                        rowData.longitude,
+                        rowData.name,
+                      )}
                   />
                 </View>
-              </View>
-            }
+              </View>}
             renderSeparator={(sectionId, rowId) =>
-              <View
-                key={rowId}
-                style={globalStyles.listRowSeparator}
+              <View key={rowId} style={globalStyles.listRowSeparator} />}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.props.isReloading}
+                onRefresh={() => {
+                  if (!this.props.isReloading) {
+                    this.props.getNearestLocations();
+                  }
+                }}
+                title="Updating..."
+                tintColor={globalColors.bevPrimary}
+                progressViewOffset={50}
+                colors={[globalColors.bevPrimary]}
               />
             }
-          refreshControl={
-            <RefreshControl
-              refreshing={this.props.isReloading}
-              onRefresh={() => {
-                if(!this.props.isReloading){
-                  this.props.getNearestLocations();
-                }
-              }}
-              title="Updating..."
-              tintColor={globalColors.bevPrimary}
-              progressViewOffset={50}
-              colors={[globalColors.bevPrimary]}
-            />
-          }
-          {...overrideProps}
+            {...overrideProps}
           />
         </View>
       </View>
