9a5f3d2f9b46b1f70c495d9ebe34b60d10f87d41
Andrew Simms <simms.andrew@gmail.com>
2017-06-15 11:35:23 -0600

    Lint

--- src/components/MainTabView.tsx
+++ src/components/MainTabView.tsx
@@ -1,25 +1,31 @@
 import * as React from "react";
-import { Component } from 'react';
-import { BackHandler, Dimensions, Text, TouchableHighlight, View } from 'react-native';
+import { Component } from "react";
+import {
+  BackHandler,
+  Dimensions,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
-import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view';
+import ScrollableTabView, { DefaultTabBar } from "react-native-scrollable-tab-view";
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 
-import {isAndroid, isIOS, WindowWidth} from '../ReactNativeUtilities';
+import { isAndroid, isIOS, WindowWidth } from "../ReactNativeUtilities";
 
-import {globalColors} from '../components/GlobalStyles';
+import { globalColors } from "../components/GlobalStyles";
 
-import {TabIconBadges} from '../containers/CMainTabView';
-import CContacts from '../containers/CContacts';
-import CBevegrams from '../containers/CBevegrams';
-import CBevegramLocations from '../containers/CBevegramLocations';
-import CHistory from '../containers/CHistory';
-import IconBadge from './IconBadge';
+import CBevegramLocations from "../containers/CBevegramLocations";
+import CBevegrams from "../containers/CBevegrams";
+import CContacts from "../containers/CContacts";
+import CHistory from "../containers/CHistory";
+import { TabIconBadges } from "../containers/CMainTabView";
+import IconBadge from "./IconBadge";
 
-import {NotificationActions} from '../db/tables';
-import store from '../configureStore'
-import {sceneOrder, sceneKeys} from '../reducers/view';
+import store from "../configureStore";
+import { NotificationActions } from "../db/tables";
+import { sceneKeys, sceneOrder } from "../reducers/view";
 
 export interface MainViewRouterProps {
   currentPage?: string;
@@ -33,88 +39,102 @@ export default class MainViewRouter extends Component<MainViewRouterProps, {}> {
   notificationListener: any;
   refreshTokenListener: any;
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    BackHandler.addEventListener('hardwareBackPress', () => {
+    BackHandler.addEventListener("hardwareBackPress", () => {
       this.props.goBackPage();
     });
   }
 
   storeFCMToken(token) {
-    store.dispatch({type: 'UPDATE_FCM_TOKEN', payload: {
-      fcmToken: token
-    }});
+    store.dispatch({
+      type: "UPDATE_FCM_TOKEN",
+      payload: {
+        fcmToken: token,
+      },
+    });
   }
 
   onNotificationClick(payload) {
-    switch(payload.action) {
+    switch (payload.action) {
       case NotificationActions.ShowNewReceivedBevegrams:
         this.showNewReceivedBevegrams();
       case NotificationActions.ShowUpcomingBirthdays:
-        store.dispatch({type: 'SEND_BEVEGRAM_TO_CONTACT_VIA_NOTIFICATION', payload: {
-          facebookId: payload.facebookId,
-        }})
+        store.dispatch({
+          type: "SEND_BEVEGRAM_TO_CONTACT_VIA_NOTIFICATION",
+          payload: {
+            facebookId: payload.facebookId,
+          },
+        });
       default:
         return;
     }
   }
 
   showNewReceivedBevegrams() {
-    store.dispatch({type: 'GO_TO_ROUTE', payload: {
-      route: "MainUi",
-    }});
-    store.dispatch({type: 'GOTO_VIEW', newScene: sceneOrder[sceneKeys.bevegrams]});
+    store.dispatch({
+      type: "GO_TO_ROUTE",
+      payload: {
+        route: "MainUi",
+      },
+    });
+    store.dispatch({
+      type: "GOTO_VIEW",
+      newScene: sceneOrder[sceneKeys.bevegrams],
+    });
   }
 
   componentDidMount() {
-    if(isAndroid){
-      const FCM = require('react-native-fcm');
+    if (isAndroid) {
+      const FCM = require("react-native-fcm");
       FCM.requestPermissions();
-      FCM.getFCMToken().then((token) => {
+      FCM.getFCMToken().then(token => {
         this.storeFCMToken(token);
-      })
+      });
 
-      this.notificationListener = FCM.on('notification', (payload) => {
+      this.notificationListener = FCM.on("notification", payload => {
         // There are 3 notification situations to handle here
         // 1. App is in the foreground
         // 2. App is in the background and notification clicked
         // (payload.opened_from_tray is 1)
         // 3. App is not running (caught in FCM.getInitialNotification)
 
-        if(payload.open_from_tray === 1){
+        if (payload.open_from_tray === 1) {
           this.onNotificationClick(payload);
         } else {
-          switch(payload.action) {
+          switch (payload.action) {
             case NotificationActions.ShowNewReceivedBevegrams:
-              store.dispatch({type: 'ADD_RECEIVED_BEVEGRAM_TO_BADGE', payload: {
-                // Should this badge show the number of bevegrams received or
-                // quantity: payload.quantity,
-                quantity: 1,
-              }})
-              store.dispatch({type: 'FETCH_RECEIVED_BEVEGRAMS'});
+              store.dispatch({
+                type: "ADD_RECEIVED_BEVEGRAM_TO_BADGE",
+                payload: {
+                  // Should this badge show the number of bevegrams received or
+                  // quantity: payload.quantity,
+                  quantity: 1,
+                },
+              });
+              store.dispatch({ type: "FETCH_RECEIVED_BEVEGRAMS" });
             case NotificationActions.ShowUpcomingBirthdays:
-              // Do other thing
+            // Do other thing
             default:
               return;
           }
         }
+      });
 
-      })
-
-      FCM.getInitialNotification().then((payload) => {
+      FCM.getInitialNotification().then(payload => {
         // getInitialNotification is clicking the notification when the app is
         // closed
         this.onNotificationClick(payload);
-      })
+      });
 
-      this.refreshTokenListener = FCM.on('refreshToken', (token) => {
+      this.refreshTokenListener = FCM.on("refreshToken", token => {
         this.storeFCMToken(token);
-      })
+      });
     }
   }
 
   componentWillUnmount() {
-    if(isAndroid){
+    if (isAndroid) {
       this.notificationListener.remove();
       this.refreshTokenListener.remove();
     }
@@ -139,16 +159,16 @@ export default class MainViewRouter extends Component<MainViewRouterProps, {}> {
       Bevegrams: "ios-beer", // Material design beer looks like trash
       Map: iconPrefix + "pin",
       History: iconPrefix + "refresh",
-    }
+    };
     // DefaultTabBar -> renderTab is a hack based on the original renderTab method to add separators between the buttons
-    return(
+    return (
       <ScrollableTabView
         renderTabBar={() => {
           return (
             <DefaultTabBar
               style={{
                 height: buttonHeight,
-                flexDirection: 'row',
+                flexDirection: "row",
                 borderWidth: 0,
                 backgroundColor: buttonBgColor,
                 elevation: 5,
@@ -164,15 +184,20 @@ export default class MainViewRouter extends Component<MainViewRouterProps, {}> {
                     onPress={() => onPressHandler(page)}
                   >
                     <View
-                      style={[{
-                        flex: 1,
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }, page !== this.props.maxScene ? {
-                        borderRightWidth: 1,
-                        borderRightColor: buttonSeparatorColor,
-                      } : {borderWidth: 0}]}
+                      style={[
+                        {
+                          flex: 1,
+                          flexDirection: "column",
+                          alignItems: "center",
+                          justifyContent: "center",
+                        },
+                        page !== this.props.maxScene
+                          ? {
+                              borderRightWidth: 1,
+                              borderRightColor: buttonSeparatorColor,
+                            }
+                          : { borderWidth: 0 },
+                      ]}
                     >
                       <IconBadge
                         containerHeight={buttonHeight}
@@ -184,32 +209,31 @@ export default class MainViewRouter extends Component<MainViewRouterProps, {}> {
                         style={{
                           color: "#ffffff",
                           fontSize: textSize * 3,
-                          backgroundColor: 'rgba(0, 0, 0, 0)'
+                          backgroundColor: "rgba(0, 0, 0, 0)",
                         }}
                       />
                       <Text
-                      style={[
-                        {
-                          fontSize: textSize,
-                          backgroundColor: 'rgba(0, 0, 0, 0.0)',
-                        },
-                        isTabActive ?
-                          {color: activeTextColor, fontWeight: 'bold'}
-                        :
-                          {color: textColor}
-                      ]}
+                        style={[
+                          {
+                            fontSize: textSize,
+                            backgroundColor: "rgba(0, 0, 0, 0.0)",
+                          },
+                          isTabActive
+                            ? { color: activeTextColor, fontWeight: "bold" }
+                            : { color: textColor },
+                        ]}
                       >
                         {name}
                       </Text>
                     </View>
                   </TouchableHighlight>
-                )
+                );
               }}
             />
-          )
+          );
         }}
         tabBarPosition="bottom"
-        onChangeTab={(input) => {
+        onChangeTab={input => {
           this.props.onPageChange(input.i);
         }}
         initialPage={0}
