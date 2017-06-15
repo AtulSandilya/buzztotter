a7e75dce9c5793135e964b106b753118edeb9194
Andrew Simms <simms.andrew@gmail.com>
2017-06-14 21:10:54 -0600

    Refactor: Add location fetch status to locationsView reducer

--- src/reducers/locationsView.ts
+++ src/reducers/locationsView.ts
@@ -1,17 +1,30 @@
 const defaultState = {
+  completedLocationFetch: false,
+  failedLocationFetch: false,
   isReloading: false,
 };
 
 export const locationsView = (state = defaultState, action) => {
   switch (action.type) {
     case "ATTEMPTING_LOCATION_NEAR_USER_UPDATE":
-      return Object.assign({}, state, {
+      return {
+        ...state,
+        completedLocationFetch: false,
+        failedLocationFetch: false,
         isReloading: true,
-      });
+      };
     case "SUCCESSFUL_LOCATION_NEAR_USER_UPDATE":
-      return Object.assign({}, state, {
+      return {
+        ...state,
+        completedLocationFetch: true,
         isReloading: false,
-      });
+      };
+    case "FAILED_LOCATION_NEAR_USER_UPDATE":
+      return {
+        ...state,
+        failedLocationFetch: true,
+        isReloading: false,
+      };
     default:
       return state;
   }
