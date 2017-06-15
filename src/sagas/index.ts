9cd37db161495fd9be573e4d11676e027b18b785
Andrew Simms <simms.andrew@gmail.com>
2017-06-14 23:18:55 -0600

    Refactor: Migrate to `redux-saga` all rootSaga pattern

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,4 +1,5 @@
 import {
+  all,
   call,
   fork,
   put,
@@ -53,125 +54,127 @@ import {Location, User} from "../db/tables";
 // Like combine reducers
 /* tslint:disable:object-literal-sort-keys */
 export default function* rootSaga() {
-  // Facebook
-  yield fork(takeEvery, "USER_FETCH_REQUESTED", facebook.fetchUser);
-  yield fork(takeEvery, "CONTACTS_FETCH_REQUESTED", facebook.fetchContacts);
-  yield fork(takeEvery, "FACEBOOK_CONTACTS_RELOAD_REQUEST", facebook.reloadContacts);
+  yield all([
+    // Facebook
+    takeEvery("USER_FETCH_REQUESTED", facebook.fetchUser),
+    takeEvery("CONTACTS_FETCH_REQUESTED", facebook.fetchContacts),
+    takeEvery("FACEBOOK_CONTACTS_RELOAD_REQUEST", facebook.reloadContacts),
 
-  // Logging In
-  yield fork(takeEvery, "SUCCESSFUL_FACEBOOK_LOGIN", facebook.successfulLogin);
-  yield fork(takeEvery, "INITIALIZE_USER_DATA_WITH_FACEBOOK_TOKEN", function *(action) {
-    // Facebook user info is required to properly initialize a firebase user
-    yield [
-      yield call(facebook.fetchContacts, action),
-      yield call(facebook.fetchUser, action),
-    ];
+    // Logging In
+    takeEvery("SUCCESSFUL_FACEBOOK_LOGIN", facebook.successfulLogin),
+    takeEvery("INITIALIZE_USER_DATA_WITH_FACEBOOK_TOKEN", function *(action) {
+      // Facebook user info is required to properly initialize a firebase user
+      yield [
+        yield call(facebook.fetchContacts, action),
+        yield call(facebook.fetchUser, action),
+      ];
 
-    yield call(firebaseFacebookLogin, action);
-    yield call(getUser);
-    if (ReactNativeUtil.isAndroid) {
-      yield call(dbWriteFcmToken);
-    }
+      yield call(firebaseFacebookLogin, action);
+      yield call(getUser);
+      if (ReactNativeUtil.isAndroid) {
+        yield call(dbWriteFcmToken);
+      }
 
-    yield call(updatePurchasePackages);
-    yield call(getLocationsNearUser);
-    yield call(updateHistory);
-  });
+      yield call(updatePurchasePackages);
+      yield call(getLocationsNearUser);
+      yield call(updateHistory);
+    }),
 
-  // Logging Out
-  yield fork(takeEvery, "REQUEST_LOGOUT", function *(action) {
-    yield call(facebook.logOutFacebook);
-    yield call(goToRoute, action);
-    yield call(firebaseLogOut, action);
-  });
+    // Logging Out
+    takeEvery("REQUEST_LOGOUT", function *(action) {
+      yield call(facebook.logOutFacebook);
+      yield call(goToRoute, action);
+      yield call(firebaseLogOut, action);
+    }),
 
-  // Handling credit cards
-  yield fork(takeEvery, "REQUEST_CREDIT_CARD_VERIFICATION", function *(action) {
-    const cardToken = yield call(fetchCreditCardToken, action);
-    if (cardToken) {
-      yield call(queue.addCreditCardToCustomer, cardToken);
+    // Handling credit cards
+    takeEvery("REQUEST_CREDIT_CARD_VERIFICATION", function *(action) {
+      const cardToken = yield call(fetchCreditCardToken, action);
+      if (cardToken) {
+        yield call(queue.addCreditCardToCustomer, cardToken);
+        yield call(
+          updateUserStateOnNextChange,
+          ["SUCCESSFUL_CREDIT_CARD_VERIFICATION", "GO_BACK_ROUTE"],
+          ["FAILED_CREDIT_CARD_VERIFICATION"],
+          "stripe.error",
+        );
+      }
+    }),
+
+    takeEvery("REQUEST_UPDATE_DEFAULT_CARD", function *(action) {
+      yield put({type: "ATTEMPTING_STRIPE_DEFAULT_CARD_UPDATE"});
+      yield call(queue.updateDefaultCard, action);
       yield call(
         updateUserStateOnNextChange,
-        ["SUCCESSFUL_CREDIT_CARD_VERIFICATION", "GO_BACK_ROUTE"],
-        ["FAILED_CREDIT_CARD_VERIFICATION"],
+        ["RENDER_SUCCESSFUL_STRIPE_DEFAULT_CARD_UPDATE"],
+        ["RENDER_FAILED_STRIPE_DEFAULT_CARD_UPDATE"],
         "stripe.error",
+        true,
       );
-    }
-  });
+    }),
 
-  yield fork(takeEvery, "REQUEST_UPDATE_DEFAULT_CARD", function *(action) {
-    yield put({type: "ATTEMPTING_STRIPE_DEFAULT_CARD_UPDATE"});
-    yield call(queue.updateDefaultCard, action);
-    yield call(
-      updateUserStateOnNextChange,
-      ["RENDER_SUCCESSFUL_STRIPE_DEFAULT_CARD_UPDATE"],
-      ["RENDER_FAILED_STRIPE_DEFAULT_CARD_UPDATE"],
-      "stripe.error",
-      true,
-    );
-  });
+    takeEvery("REQUEST_REMOVE_CARD", function *(action) {
+      yield put({type: "ATTEMPTING_STRIPE_REMOVE_CARD"});
+      yield call(queue.removeCreditCardFromCustomer, action);
+      yield call(
+        updateUserStateOnNextChange,
+        ["RENDER_SUCCESSFUL_STRIPE_REMOVE_CARD"],
+        ["RENDER_FAILED_STRIPE_REMOVE_CARD"],
+        "stripe.error",
+        true,
+      );
+    }),
 
-  yield fork(takeEvery, "REQUEST_REMOVE_CARD", function *(action) {
-    yield put({type: "ATTEMPTING_STRIPE_REMOVE_CARD"});
-    yield call(queue.removeCreditCardFromCustomer, action);
-    yield call(
-      updateUserStateOnNextChange,
-      ["RENDER_SUCCESSFUL_STRIPE_REMOVE_CARD"],
-      ["RENDER_FAILED_STRIPE_REMOVE_CARD"],
-      "stripe.error",
-      true,
-    );
-  });
+    takeEvery("PURCHASE_REQUEST_UPDATE_USER", function *() {
+      yield put({type: "ATTEMPTING_USER_REFRESH_FOR_PURCHASE"});
+      yield call(getUser);
+      yield call(updatePurchasePackages);
+      yield put({type: "COMPLETED_USER_REFRESH_FOR_PURCHASE"});
+    }),
 
-  yield fork(takeEvery, "PURCHASE_REQUEST_UPDATE_USER", function *() {
-    yield put({type: "ATTEMPTING_USER_REFRESH_FOR_PURCHASE"});
-    yield call(getUser);
-    yield call(updatePurchasePackages);
-    yield put({type: "COMPLETED_USER_REFRESH_FOR_PURCHASE"});
-  });
+    // Purchasing Then Sending
+    takeEvery("PURCHASE_THEN_SEND_BEVEGRAM", function *(action) {
+      yield call(goToRoute, action);
+      yield call(queue.purchase, action);
 
-  // Purchasing Then Sending
-  yield fork(takeEvery, "PURCHASE_THEN_SEND_BEVEGRAM", function *(action) {
-    yield call(goToRoute, action);
-    yield call(queue.purchase, action);
+      yield call(listenUntilPurchaseSuccessOrFailure);
+      yield call(updateHistory);
+    }),
 
-    yield call(listenUntilPurchaseSuccessOrFailure);
-    yield call(updateHistory);
-  });
+    // Receive Bevegrams
+    takeEvery("FETCH_RECEIVED_BEVEGRAMS", function *(action){
+      yield put({type: "ATTEMPTING_UPDATE_RECEIVED_BEVEGRAMS"});
+      const receivedBevegrams = yield call(updateReceivedBevegrams);
+      yield put({type: "UPDATE_RECEIVED_BEVEGRAMS", payload: {
+        receivedBevegrams: receivedBevegrams,
+      }});
+      yield put({type: "SUCCESSFUL_UPDATE_RECEIVED_BEVEGRAMS"});
+    }),
 
-  // Receive Bevegrams
-  yield fork(takeEvery, "FETCH_RECEIVED_BEVEGRAMS", function *(action){
-    yield put({type: "ATTEMPTING_UPDATE_RECEIVED_BEVEGRAMS"});
-    const receivedBevegrams = yield call(updateReceivedBevegrams);
-    yield put({type: "UPDATE_RECEIVED_BEVEGRAMS", payload: {
-      receivedBevegrams: receivedBevegrams,
-    }});
-    yield put({type: "SUCCESSFUL_UPDATE_RECEIVED_BEVEGRAMS"});
-  });
+    // Redeem Bevegram
+    takeEvery("REDEEM_BEVEGRAM", function *(action){
+      yield put({type: "ATTEMPTING_REDEEM"});
+      const redeemLocation: Location = yield call(getLocationsAtUserLocation);
+      yield call(queue.redeem, action, redeemLocation);
+      yield call(listenUntilRedeemSuccessOrFailure);
+      yield call(updateHistory);
+      yield put({type: "FINISHED_REDEEM"});
+    }),
 
-  // Redeem Bevegram
-  yield fork(takeEvery, "REDEEM_BEVEGRAM", function *(action){
-    yield put({type: "ATTEMPTING_REDEEM"});
-    const redeemLocation: Location = yield call(getLocationsAtUserLocation);
-    yield call(queue.redeem, action, redeemLocation);
-    yield call(listenUntilRedeemSuccessOrFailure);
-    yield call(updateHistory);
-    yield put({type: "FINISHED_REDEEM"});
-  });
+    // Routes
+    takeEvery("GO_TO_ROUTE", goToRoute),
+    takeEvery("GO_BACK_ROUTE", goBackRoute),
+    // Dispatch actions based on router events
+    takeEvery(ActionConst.FOCUS, onFocusRoute),
 
-  // Routes
-  yield fork(takeEvery, "GO_TO_ROUTE", goToRoute);
-  yield fork(takeEvery, "GO_BACK_ROUTE", goBackRoute);
-  // Dispatch actions based on router events
-  yield fork(takeEvery, ActionConst.FOCUS, onFocusRoute);
+    // Notifications
+    takeEvery("UPDATE_FCM_TOKEN", storeFcmToken),
 
-  // Notifications
-  yield fork(takeEvery, "UPDATE_FCM_TOKEN", storeFcmToken);
+    // History
+    takeEvery("REFRESH_HISTORY", updateHistory),
 
-  // History
-  yield fork(takeEvery, "REFRESH_HISTORY", updateHistory);
-
-  // Locations Near User
-  yield fork(takeEvery, "REQUEST_LOCATIONS_NEAR_USER", getLocationsNearUser);
-  yield fork(takeEvery, "REQUEST_BAR_AT_USER_LOCATION", getLocationsAtUserLocation);
+    // Locations Near User
+    takeEvery("REQUEST_LOCATIONS_NEAR_USER", getLocationsNearUser),
+    takeEvery("REQUEST_BAR_AT_USER_LOCATION", getLocationsAtUserLocation),
+  ]);
 }
