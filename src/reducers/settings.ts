3bd5ae55a2e9ebab3b42b9b963b90634f4cc3db8
Andrew Simms <simms.andrew@gmail.com>
2017-06-14 21:13:57 -0600

    Lint

--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -1,19 +1,19 @@
 export const settingsKeys = {
-  notifications: 'notifications',
-  location: 'location',
-}
+  location: "location",
+  notifications: "notifications",
+};
 
 const defaultSettings = {
-  notifications: true,
   location: true,
-}
+  notifications: true,
+};
 
 export const settings = (state = defaultSettings, action) => {
-  switch(action.type){
-    case 'TOGGLE_SETTING':
+  switch (action.type) {
+    case "TOGGLE_SETTING":
       // Check if the settingKey passed is valid, if not return the original
       // state, ie don't toggle anything
-      if(settingsKeys.hasOwnProperty(action.settingKey)){
+      if (settingsKeys.hasOwnProperty(action.settingKey)) {
         return toggleSetting(state, action.settingKey);
       } else {
         return state;
@@ -21,11 +21,11 @@ export const settings = (state = defaultSettings, action) => {
     default:
       return state;
   }
-}
+};
 
 const toggleSetting = (state, key) => {
   // Create a cloned state object and assign the key the inverted value from the state
-  let newState = Object.assign({}, state);
+  const newState = { ...state };
   newState[key] = !state[key];
   return newState;
-}
+};
