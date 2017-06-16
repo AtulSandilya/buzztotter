02a6eb0cbcdd1d7c53af327ca261b3754f011daf
Andrew Simms <simms.andrew@gmail.com>
2017-06-15 22:47:35 -0600

    Lint

--- src/reducers/badges.ts
+++ src/reducers/badges.ts
@@ -1,4 +1,4 @@
-import {sceneOrder, sceneKeys} from './view';
+import { sceneKeys, sceneOrder } from "./view";
 
 interface BadgesState {
   unseenReceivedBevegrams: number;
@@ -8,28 +8,31 @@ interface BadgesState {
 const initialState: BadgesState = {
   unseenReceivedBevegrams: 0,
   upcomingBirthdays: 0,
-}
+};
 
 export const badges = (state: BadgesState = initialState, action) => {
-  switch(action.type){
-    case 'ADD_RECEIVED_BEVEGRAM_TO_BADGE':
-      return Object.assign({}, state, {
-        unseenReceivedBevegrams: state.unseenReceivedBevegrams + action.payload.quantity,
-      })
-    case 'RESET_BADGE':
-      if(action.payload){
+  switch (action.type) {
+    case "ADD_RECEIVED_BEVEGRAM_TO_BADGE":
+      return {
+        ...state,
+        unseenReceivedBevegrams:
+          state.unseenReceivedBevegrams + action.payload.quantity,
+      };
+    case "RESET_BADGE":
+      if (action.payload) {
         const newScenePosition = action.payload.newScenePosition;
-        switch(newScenePosition) {
+        switch (newScenePosition) {
           case sceneOrder[sceneKeys.bevegrams]:
-            return Object.assign({}, state, {
+            return {
+              ...state,
               unseenReceivedBevegrams: 0,
-            })
+            };
           default:
-            return state
+            return state;
         }
       }
       return state;
     default:
       return state;
   }
-}
+};
