b7358fbf7268b95a05e3ca7cf258521a860c8142
Andrew Simms <simms.andrew@gmail.com>
2017-06-19 18:29:10 -0600

    Ui: Remove bevegram badge when the user moves to a new tab
    Previously the badge was removed when the entered the bevegrams tab.
    This meant the badge number disappeared before the user could see the
    number with the view as context. Mirrors how the iOS phone app handles
    this type of badge.

--- src/reducers/badges.ts
+++ src/reducers/badges.ts
@@ -3,12 +3,16 @@ import { sceneKeys, sceneOrder } from "./view";
 interface BadgesState {
   unseenReceivedBevegrams: number;
   upcomingBirthdays: number;
+  resetBadgeOnNextClick: boolean;
 }
 
 const initialState: BadgesState = {
+  resetBadgeOnNextClick: false,
   unseenReceivedBevegrams: 0,
   upcomingBirthdays: 0,
 };
+
+const sceneStack = [];
 
 export const badges = (state: BadgesState = initialState, action) => {
   switch (action.type) {
@@ -19,19 +23,29 @@ export const badges = (state: BadgesState = initialState, action) => {
           state.unseenReceivedBevegrams + action.payload.quantity,
       };
     case "RESET_BADGE":
-      if (action.payload) {
-        const newScenePosition = action.payload.newScenePosition;
-        switch (newScenePosition) {
-          case sceneOrder[sceneKeys.bevegrams]:
-            return {
-              ...state,
-              unseenReceivedBevegrams: 0,
-            };
-          default:
-            return state;
-        }
+      const newScenePosition = action.payload.newScenePosition;
+      if (newScenePosition !== sceneStack[0]) {
+        sceneStack.unshift(newScenePosition);
       }
-      return state;
+
+      if (
+        state.resetBadgeOnNextClick ||
+        (sceneStack.length >= 1 &&
+          sceneStack[1] === sceneOrder[sceneKeys.bevegrams])
+      ) {
+        return {
+          ...state,
+          unseenReceivedBevegrams: 0,
+        };
+      } else {
+        return state;
+      }
+
+    case "RESET_BADGE_ON_NEXT_CLICK":
+      return {
+        ...state,
+        resetBadgeOnNextClick: true,
+      };
     default:
       return state;
   }
