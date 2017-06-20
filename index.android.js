94588c2b1d6f88ecab827ab64c5fed240784772d
Andrew Simms <simms.andrew@gmail.com>
2017-06-19 18:18:37 -0600

    Refactor: Update android package name

--- index.android.js
+++ index.android.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 
 import Bevegram from './build/app.js';
 
-AppRegistry.registerComponent('Bevegram', () => Bevegram);
+AppRegistry.registerComponent('BuzzOtter', () => Bevegram);
