0e274060cccf5f962495b8e290ad062edac0b5ed
Andrew Simms <simms.andrew@gmail.com>
2017-06-23 11:17:09 -0600

    Lint

--- src/containers/CPurchaseBevegram.ts
+++ src/containers/CPurchaseBevegram.ts
@@ -1,11 +1,13 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import PurchaseBevegram from '../components/PurchaseBevegram';
+import PurchaseBevegram from "../components/PurchaseBevegram";
 
-const mapStateToProps = (state) => {
-  const routeData = Object.assign({},
-    state.routes.PurchaseBevegram.data,
-    state.routes.SendBevegram.data);
+/* tslint:disable:object-literal-sort-keys */
+const mapStateToProps = state => {
+  const routeData = {
+    ...state.routes.PurchaseBevegram.data,
+    ...state.routes.SendBevegram.data,
+  };
 
   return {
     fullName: state.routes.SendBevegram.data.fullName,
@@ -15,75 +17,97 @@ const mapStateToProps = (state) => {
     facebookId: state.routes.SendBevegram.data.facebookId,
     purchase: state.purchase,
     creditCards: state.user.stripe ? state.user.stripe.creditCards : [],
-    activeCardId: state.user.stripe ? state.user.stripe.activeCardId : undefined,
+    activeCardId: state.user.stripe
+      ? state.user.stripe.activeCardId
+      : undefined,
     attemptingUpdate: state.purchase.attemptingStripeUpdate,
     attemptingVerification: state.addCreditCard.attemptingVerification,
     purchasePackages: state.purchase.purchasePackages,
-    selectedPurchasePackage: state.purchase.purchasePackages[state.purchase.selectedPurchasePackageIndex],
+    selectedPurchasePackage:
+      state.purchase.purchasePackages[
+        state.purchase.selectedPurchasePackageIndex
+      ],
     selectedPurchasePackageIndex: state.purchase.selectedPurchasePackageIndex,
     attemptingSend: state.purchase.attemptingSend,
     completedSend: state.purchase.completedSend,
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     resetPurchase: () => {
-      dispatch({type: 'RESET_CREDIT_CARD_PURCHASE'});
+      dispatch({ type: "RESET_CREDIT_CARD_PURCHASE" });
     },
     closePurchaseRoute: () => {
-      dispatch({type: 'GO_BACK_ROUTE'});
+      dispatch({ type: "GO_BACK_ROUTE" });
     },
     startCreditCardPurchase: (purchaseData, inProgressData) => {
-      dispatch({type: 'PURCHASE_BEVEGRAM', payload: {
-        purchaseData: purchaseData,
-        routeData: inProgressData,
-        route: "PurchaseInProgress",
-      }});
+      dispatch({
+        type: "PURCHASE_BEVEGRAM",
+        payload: {
+          purchaseData: purchaseData,
+          routeData: inProgressData,
+          route: "PurchaseInProgress",
+        },
+      });
     },
     sendBevegram: (sendBevegramData, inProgressData) => {
-      dispatch({type: 'SEND_BEVEGRAM', payload: {
-        sendBevegramData: sendBevegramData,
-        routeData: inProgressData,
-        route: "SendInProgress",
-      }})
+      dispatch({
+        type: "SEND_BEVEGRAM",
+        payload: {
+          sendBevegramData: sendBevegramData,
+          routeData: inProgressData,
+          route: "SendInProgress",
+        },
+      });
     },
     purchaseAndSend: (purchaseData, sendBevegramData, inProgressData) => {
-      dispatch({type: 'PURCHASE_THEN_SEND_BEVEGRAM', payload: {
-        purchaseData: purchaseData,
-        sendBevegramData: sendBevegramData,
-        routeData: inProgressData,
-        route: "SendInProgress",
-      }})
+      dispatch({
+        type: "PURCHASE_THEN_SEND_BEVEGRAM",
+        payload: {
+          purchaseData: purchaseData,
+          sendBevegramData: sendBevegramData,
+          routeData: inProgressData,
+          route: "SendInProgress",
+        },
+      });
     },
     goToAddCreditCardRoute: () => {
-      dispatch({type: 'GO_TO_ROUTE', payload: {route: "AddCreditCard"}});
+      dispatch({ type: "GO_TO_ROUTE", payload: { route: "AddCreditCard" } });
     },
     removeCard: (cardId, cardIndex) => {
-      dispatch({type: 'REQUEST_REMOVE_CARD', payload: {
-        cardToDelete: cardId,
-        cardIndex: cardIndex,
-      }})
+      dispatch({
+        type: "REQUEST_REMOVE_CARD",
+        payload: {
+          cardToDelete: cardId,
+          cardIndex: cardIndex,
+        },
+      });
     },
     updateDefaultCard: (newDefaultCardId: string) => {
-      dispatch({type: 'REQUEST_UPDATE_DEFAULT_CARD', payload: {
-        newDefaultCard: newDefaultCardId,
-      }})
+      dispatch({
+        type: "REQUEST_UPDATE_DEFAULT_CARD",
+        payload: {
+          newDefaultCard: newDefaultCardId,
+        },
+      });
     },
     selectPackage: (newSelectedPurchasePackageIndex: string) => {
-      dispatch({type: 'SELECT_PURCHASE_PACKAGE', payload: {
-        newSelectedPurchasePackageIndex: newSelectedPurchasePackageIndex,
-      }})
+      dispatch({
+        type: "SELECT_PURCHASE_PACKAGE",
+        payload: {
+          newSelectedPurchasePackageIndex: newSelectedPurchasePackageIndex,
+        },
+      });
     },
     getUser: () => {
-      dispatch({type: 'PURCHASE_REQUEST_UPDATE_USER'});
-    }
-  }
-}
+      dispatch({ type: "PURCHASE_REQUEST_UPDATE_USER" });
+    },
+  };
+};
 
-const CPurchaseBevegram = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(PurchaseBevegram);
+const CPurchaseBevegram = connect(mapStateToProps, mapDispatchToProps)(
+  PurchaseBevegram,
+);
 
 export default CPurchaseBevegram;
