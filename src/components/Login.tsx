83b9187fb86c953af1e3cc2234e28cdbe79aeaed
Andrew Simms <simms.andrew@gmail.com>
2017-06-30 00:22:53 -0600

    Lint

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { Component } from 'react';
-import { Image, Text, TouchableHighlight, View } from 'react-native';
+import { Component } from "react";
+import { Image, Text, TouchableHighlight, View } from "react-native";
 
-import CFacebookLoginButton from '../containers/CFacebookLoginButton';
+import CFacebookLoginButton from "../containers/CFacebookLoginButton";
 
-import {globalColors} from './GlobalStyles';
-import {WindowHeight, WindowWidth} from '../ReactNativeUtilities';
+import { WindowHeight, WindowWidth } from "../ReactNativeUtilities";
+import { globalColors } from "./GlobalStyles";
 
 export interface LoginProps {
   isLoggedIn: boolean;
@@ -13,45 +13,64 @@ export interface LoginProps {
   requestFacebookData(token: string): void;
 }
 
+/* tslint:disable:no-magic-numbers */
 const Login: React.StatelessComponent<LoginProps> = ({
   isLoggedIn,
   onSuccessfulFacebookLogin,
   requestFacebookData,
-}) => (
-  <View style={{flex: 1}}>
-    <View style={{flex: 2, alignItems: 'center', justifyContent: 'center', backgroundColor: globalColors.bevPrimary}}>
+}) =>
+  <View
+    style={{
+      flex: 1,
+    }}
+  >
+    <View
+      style={{
+        alignItems: "center",
+        backgroundColor: globalColors.bevPrimary,
+        flex: 2,
+        justifyContent: "center",
+      }}
+    >
       <Image
-        source={require('../../img/logos/logo-on-brown-big.png')}
+        source={require("../../img/logos/logo-on-brown-big.png")}
         style={{
-          width: WindowWidth * 0.8,
           height: WindowHeight * 0.3,
+          width: WindowWidth * 0.8,
         }}
         resizeMode="contain"
       />
-      <Text style={{
-        fontSize: 25,
-        marginTop: 35,
-        color: '#333',
-      }}
+      <Text
+        style={{
+          color: "#333",
+          fontSize: 25,
+          marginTop: 35,
+        }}
       >
         Sending Drinks Made Easy!
       </Text>
     </View>
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <View style={{flex: -1, alignItems: 'center'}}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: -1, alignItems: "center" }}>
         <CFacebookLoginButton
           onLoginSuccessful={onSuccessfulFacebookLogin}
           onGetTokenSuccessful={requestFacebookData}
         />
       </View>
-      <View style={{flex: -1, alignItems: 'center', marginTop: 30, marginHorizontal: 30}}>
+      <View
+        style={{
+          alignItems: "center",
+          flex: -1,
+          marginHorizontal: 30,
+          marginTop: 30,
+        }}
+      >
         <Text>
           * We only use your Contacts to send and receive Bevegrams.
-          Your privacy is important to us. We promise to never share your information with anyone for any reason.
+          Your privacy is important to us. We promise to never share your
+          information with anyone for any reason.
         </Text>
       </View>
     </View>
-  </View>
-)
-
+  </View>;
 export default Login;
