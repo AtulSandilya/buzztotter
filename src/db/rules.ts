f51d19048e87141a08b6d60c5a4b393abcfd3a23
Andrew Simms <simms.andrew@gmail.com>
2017-07-03 12:01:55 -0600

    Refactor: Remove previous db rules file

--- src/db/rules.ts
+++
@@ -1,9 +0,0 @@
-const dbRules =
-// To change the database rules paste this into the firebase console ->
-// database -> rules section
-{
-  "rules": {
-    ".read": "auth != null",
-    ".write": "auth != null"
-  }
-}
