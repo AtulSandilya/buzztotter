bdab556676d294808c2ee3d1820db13ffc7fa844
Andrew Simms <simms.andrew@gmail.com>
2017-08-01 14:03:02 -0600

    Utils: Add `CalcSuccessFailColor` for dynamic red to green ui components

--- src/ReactNativeUtilities.ts
+++ src/ReactNativeUtilities.ts
@@ -1,5 +1,9 @@
 import { Dimensions, Platform, StatusBar } from "react-native";
 
+import Color from "color";
+
+import theme from "./theme";
+
 export const isAndroid = Platform.OS === "android";
 export const isIOS = Platform.OS === "ios";
 
@@ -13,3 +17,35 @@ const iOSStatusBarHeight = 20;
 export const StatusBarHeight = isIOS
   ? iOSStatusBarHeight
   : isAndroid ? StatusBar.currentHeight : iOSStatusBarHeight;
+
+// Returns one hex color between two colors: `startColor` -> fully desaturated
+// `startColor` -> fully desaturated `endColor` -> `endColor`
+// `startColorShift` determines when the `input` shifts from the start color
+// to the end color. The distance from start color shift determines the amount
+// of saturation either the start or end color.
+const CalculateTwoColorShift = (
+  input: number,
+  startColorShift: number,
+  startColor: string,
+  endColor: string,
+) => {
+  if (input < startColorShift) {
+    return Color(startColor).desaturate(input / startColorShift).hex();
+  } else {
+    return Color(endColor).desaturate(startColorShift / input).hex();
+  }
+};
+
+export const CalcSuccessFailColor = (
+  input: number,
+  startRed: number,
+  useBoldRed?: boolean,
+): string => {
+  const failureRed = useBoldRed ? theme.colors.failure : theme.colors.failureBg;
+  return CalculateTwoColorShift(
+    input,
+    startRed,
+    theme.colors.successBg,
+    failureRed,
+  );
+};
