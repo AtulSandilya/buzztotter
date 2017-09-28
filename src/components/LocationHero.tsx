c1df85707e093327c0496a8cabfafee426ab1e1e
Andrew Simms <simms.andrew@gmail.com>
2017-09-27 23:15:42 -0600

    Refactor: Use `LocationAddressAndDistance` in `LocationHero`

--- src/components/LocationHero.tsx
+++ src/components/LocationHero.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Text, TouchableHighlight, View } from "react-native";
+import { View } from "react-native";
 
-import { PrettyFormatDistance } from "../CommonUtilities";
 import { Location } from "../db/tables";
+import theme from "../theme";
 
-import BevUiText from "./BevUiText";
-import { globalStyles } from "./GlobalStyles";
-import { OpenMapsAppToAddress } from "./LocationDetail";
+import BevText from "./BevText";
+import LocationAddressAndDistance from "./LocationAddressAndDistance";
 
 interface LocationHeroProps {
   loc: Location;
@@ -15,29 +14,14 @@ interface LocationHeroProps {
 const LocationHero: React.StatelessComponent<LocationHeroProps> = props => {
   return (
     <View>
-      <Text style={globalStyles.heroText}>{props.loc.name}</Text>
-      <TouchableHighlight
-        onPress={() =>
-          OpenMapsAppToAddress(
-            props.loc.latitude,
-            props.loc.longitude,
-            props.loc.name,
-          )}
-        underlayColor="#ffffff"
+      <BevText
+        fontWeight="light"
+        size="extraExtraLarge"
+        color={theme.colors.uiTextColor}
       >
-        <View>
-          <BevUiText icon={"globe"} style={{ paddingBottom: 4 }}>
-            {props.loc.address}
-          </BevUiText>
-        </View>
-      </TouchableHighlight>
-      <BevUiText icon="map-marker">
-        {PrettyFormatDistance(
-          props.loc.distanceFromUser,
-          "imperial",
-          props.loc.squareFootage,
-        )}
-      </BevUiText>
+        {props.loc.name}
+      </BevText>
+      <LocationAddressAndDistance loc={props.loc} paddingTop={0} />
     </View>
   );
 };
