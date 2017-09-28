f2fea06e5b30a5466e0f63b76993c7591fe876da
Andrew Simms <simms.andrew@gmail.com>
2017-09-28 12:47:50 -0600

    Ui: Refactor `RedeemComplete` with `Bev` components
    Add a `View` wrapper around everything to pass a single child to
    `RouteWithNavBarWrapper`

--- src/components/RedeemComplete.tsx
+++ src/components/RedeemComplete.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
-import { Alert, Text, View } from "react-native";
+import { Alert, View } from "react-native";
 
 import moment from "moment";
 import * as Animatable from "react-native-animatable";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 import theme from "../theme";
 
@@ -11,8 +10,10 @@ import { Pluralize } from "../CommonUtilities";
 import { Location } from "../db/tables";
 import { WindowWidth } from "../ReactNativeUtilities";
 
+import BevButton from "./BevButton";
+import BevIcon, { IconType } from "./BevIcon";
+import BevText from "./BevText";
 import BevTimestamp from "./BevTimestamp";
-import BevUiButton from "./BevUiButton";
 import BevUiText from "./BevUiText";
 import RouteWithNavBarWrapper from "./RouteWithNavBarWrapper";
 
@@ -60,96 +61,108 @@ class RedeemComplete extends React.Component<RedeemCompleteProps, {}> {
     const containerPadding = 15;
     return (
       <RouteWithNavBarWrapper>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-            padding: containerPadding,
-            paddingTop: 0,
-          }}
-        >
-          <View style={{ paddingVertical: theme.padding.default }}>
-            <Text style={[globalStyles.heroText, { fontWeight: "bold" }]}>
-              Redeem Successful!
-            </Text>
-            <BevUiText icon="clock-o">
-              {moment(this.props.redeemCompletedTime).format(
-                "dddd, MMMM Do YYYY, h:mm:ss a",
-              )}
-            </BevUiText>
-          </View>
+        <View>
           <View
             style={{
-              alignItems: "center",
               flex: 1,
-              justifyContent: "center",
-              marginBottom: theme.padding.default,
+              flexDirection: "column",
+              padding: containerPadding,
+              paddingTop: 0,
             }}
           >
+            <View style={{ paddingVertical: theme.padding.normal }}>
+              <BevText
+                size="extraExtraLarge"
+                fontWeight="bold"
+                color={theme.colors.uiBoldTextColor}
+              >
+                Redeem Successful!
+              </BevText>
+              <BevUiText icon="time">
+                {moment(this.props.redeemCompletedTime).format(
+                  "dddd, MMMM Do YYYY, h:mm:ss a",
+                )}
+              </BevUiText>
+            </View>
             <View
               style={{
-                height: this.animationSize,
-                width: this.animationSize,
+                alignItems: "center",
+                flex: 1,
+                justifyContent: "center",
+                marginBottom: theme.padding.normal,
               }}
             >
               <View
                 style={{
-                  alignItems: "center",
-                  flex: 1,
-                  justifyContent: "center",
-                  marginVertical: containerPadding,
+                  height: this.animationSize,
+                  width: this.animationSize,
                 }}
               >
-                <Animatable.Image
-                  animation="rotate"
-                  duration={this.animationDuration}
-                  easing="ease-out-quint"
-                  iterationCount="infinite"
-                  source={require("../../img/logos/android-just-otter-icon.png")}
+                <View
                   style={{
-                    height: this.animationSize,
-                    width: this.animationSize,
+                    alignItems: "center",
+                    flex: 1,
+                    justifyContent: "center",
+                    marginVertical: containerPadding,
                   }}
-                  useNativeDriver={true}
+                >
+                  <Animatable.Image
+                    animation="rotate"
+                    duration={this.animationDuration}
+                    easing="ease-out-quint"
+                    iterationCount="infinite"
+                    source={require("../../img/logos/android-just-otter-icon.png")}
+                    style={{
+                      height: this.animationSize,
+                      width: this.animationSize,
+                    }}
+                    useNativeDriver={true}
+                  />
+                </View>
+              </View>
+            </View>
+            <View style={{ alignItems: "center" }}>
+              <BevUiText
+                color={globalColors.lightText}
+                fontSize="largeNormal"
+                icon="alert"
+                preserveCase={true}
+                style={{ marginBottom: theme.padding.normal }}
+              >
+                {"Don't forget to tip your bartender or server!"}
+              </BevUiText>
+            </View>
+            <HeroLine
+              icon="redeem"
+              text={`${this.props.quantity} Bevegram${Pluralize(
+                this.props.quantity,
+              )}`}
+            />
+            <HeroLine icon="address" text={this.props.loc.name} />
+            <HeroLine icon="time">
+              <BevTimestamp
+                colorThreshold={this.startRedTime}
+                date={this.props.redeemCompletedTime}
+                size={"extraLarge"}
+                preserveCase={true}
+                hero={true}
+                hideIcon={true}
+              />
+            </HeroLine>
+            <View style={globalStyles.bevLineNoSepWithMargin}>
+              <View style={globalStyles.bevLineRight}>
+                <BevButton
+                  text="Done"
+                  shortText="Done"
+                  iconType="success"
+                  onPress={this.showConfirmRedeemAlert}
                 />
               </View>
             </View>
           </View>
-          <View style={{ alignItems: "center" }}>
-            <BevUiText
-              color={globalColors.lightText}
-              fontSize="large"
-              icon="warning"
-              preserveCase={true}
-              style={{ marginBottom: theme.padding.default }}
-            >
-              {"Don't forget to tip your bartender or server!"}
-            </BevUiText>
-          </View>
-          <HeroLine
-            icon="beer"
-            text={`${this.props.quantity} Bevegram${Pluralize(
-              this.props.quantity,
-            )}`}
-          />
-          <HeroLine icon="globe" text={this.props.loc.name} />
-          <HeroLine icon="clock-o">
-            <BevTimestamp
-              colorThreshold={this.startRedTime}
-              date={this.props.redeemCompletedTime}
-              size={"massive"}
-              preserveCase={true}
-              hero={true}
-            />
-          </HeroLine>
-          <BevUiButton
-            text="Done"
-            color="green"
-            onPress={this.showConfirmRedeemAlert}
-          />
+          {/* Ensure the Button can be scrolled to */}
+          <View style={{ height: 150 }} />
         </View>
-        {/* Ensure the Button can be scrolled to */}
-        <View style={{ height: 150 }} />
       </RouteWithNavBarWrapper>
     );
   }
@@ -157,23 +170,24 @@ class RedeemComplete extends React.Component<RedeemCompleteProps, {}> {
 
 interface HeroLineProps {
   text?: string;
-  icon?: string;
-  children?: React.ReactChildren;
+  icon?: IconType;
+  children?: any;
 }
 
 const HeroLine: React.StatelessComponent<HeroLineProps> = props => {
   const heroMargin = 15;
+  const heroTextSize = "extraLarge";
 
   if (props.text && props.icon) {
     return (
       <View style={[globalStyles.bevLine, { marginBottom: heroMargin }]}>
         <View style={globalStyles.bevLineLeft}>
-          <FontAwesome name={props.icon} style={globalStyles.heroText} />
+          <BevIcon iconType={props.icon} size={heroTextSize} />
         </View>
         <View style={globalStyles.bevLineRight}>
-          <Text style={globalStyles.smallerHeroText} numberOfLines={1}>
+          <BevText size={heroTextSize} fontWeight="light" numberOfLines={1}>
             {props.text}
-          </Text>
+          </BevText>
         </View>
       </View>
     );
@@ -181,7 +195,7 @@ const HeroLine: React.StatelessComponent<HeroLineProps> = props => {
     return (
       <View style={[globalStyles.bevLine, { marginBottom: heroMargin }]}>
         <View style={globalStyles.bevLineLeft}>
-          <FontAwesome name={props.icon} style={globalStyles.heroText} />
+          <BevIcon iconType={props.icon} size={heroTextSize} />
         </View>
         <View style={globalStyles.bevLineRight}>
           {props.children}
@@ -191,7 +205,9 @@ const HeroLine: React.StatelessComponent<HeroLineProps> = props => {
   } else {
     return (
       <View style={[globalStyles.bevLine, { marginBottom: heroMargin }]}>
-        <Text style={globalStyles.smallerHeroText}>{props.text}</Text>
+        <BevText size={heroTextSize} fontWeight="light" numberOfLines={1}>
+          {props.text}
+        </BevText>
       </View>
     );
   }
