5d2f7d334d06c9735a7030bac34eba05e58c0b84
Andrew Simms <simms.andrew@gmail.com>
2017-09-27 23:19:53 -0600

    Refactor: Drastically simplify `BevUiButton`
    Now that all button functions have been moved to `BevButton`,
    `BevUiButton` just needs to a be a `TouchableHighlight` wrapper
    around `BevUiText`.

--- src/components/BevUiButton.tsx
+++ src/components/BevUiButton.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
-import { TouchableHighlight, View, ViewStyle } from "react-native";
+import { TouchableHighlight, View } from "react-native";
 
 import theme from "../theme";
 
-import BevUiText from "./BevUiText";
+import BevUiText, { BevUiTextProps } from "./BevUiText";
 
-import { globalStyles } from "./GlobalStyles";
-
-interface BevUiButtonProps {
+interface BevUiButtonProps extends BevUiTextProps {
   onPress: () => void;
-  text: string;
-  icon?: string;
-  style?: ViewStyle;
-  color?: string;
-  left?: boolean;
 }
 
 const BevUiButton: React.StatelessComponent<BevUiButtonProps> = props => {
@@ -21,22 +14,12 @@ const BevUiButton: React.StatelessComponent<BevUiButtonProps> = props => {
     <TouchableHighlight
       onPress={props.onPress}
       underlayColor={"#ffffff"}
-      style={[
-        props.left ? globalStyles.bevLineLeft : globalStyles.bevLineRight,
-        props.style,
-      ]}
+      style={{ padding: theme.padding.small }}
     >
       <View style={{ flex: -1 }}>
-        <BevUiText
-          icon={props.icon ? props.icon : "check"}
-          iconRight={props.left ? false : true}
-          iconBold={true}
-          fontSize={"massive"}
-          isButton={true}
-          text={props.text}
-          fontStyle={{ fontWeight: "bold" }}
-          color={props.color ? props.color : theme.colors.text}
-        />
+        <BevUiText {...props}>
+          {props.children}
+        </BevUiText>
       </View>
     </TouchableHighlight>
   );
