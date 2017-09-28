8a9549e24133673d6ac431ba3aabe46a1e99baaf
Andrew Simms <simms.andrew@gmail.com>
2017-09-28 12:40:39 -0600

    Refactor: Use `LocationAddressAndDistance` in `RedeemLocationChoiceLine`
    Also refactor with `Bev` components.

--- src/components/RedeemLocationChoiceLine.tsx
+++ src/components/RedeemLocationChoiceLine.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
 import { View } from "react-native";
 
+import { Location } from "../db/tables";
 import theme from "../theme";
-import BevPressableLine from "./BevPressableLine";
-import BevUiText from "./BevUiText";
 
-import {
-  PrettyFormatDistance,
-  SquareFootageToRadius,
-} from "../CommonUtilities";
-import { DEFAULT_SQUARE_FOOTAGE, Location } from "../db/tables";
-import { CalcSuccessFailColor } from "../ReactNativeUtilities";
+import BevLargerTitleText from "./BevLargerTitleText";
+import BevPressableLine from "./BevPressableLine";
+import LocationAddressAndDistance from "./LocationAddressAndDistance";
 
 interface RedeemLocationChoiceLineProps {
   loc: Location | undefined;
@@ -19,14 +15,6 @@ interface RedeemLocationChoiceLineProps {
   other?: boolean;
   onPress(): void;
 }
-
-const CalcColorFromDistance = (
-  distance: number,
-  squareFootage: number = DEFAULT_SQUARE_FOOTAGE,
-): string => {
-  const startRedDistance = SquareFootageToRadius(squareFootage);
-  return CalcSuccessFailColor(distance, startRedDistance);
-};
 
 const RedeemLocationChoiceLine: React.StatelessComponent<
   RedeemLocationChoiceLineProps
@@ -37,69 +25,20 @@ const RedeemLocationChoiceLine: React.StatelessComponent<
     ? isLoadingText
     : props.loc ? `${props.index}. ${props.loc.name}` : errorText;
 
-  const addressText = props.isLoading
-    ? "..."
-    : props.loc ? props.loc.address : "?";
-
-  const distanceText = props.isLoading
-    ? "..."
-    : props.loc
-      ? PrettyFormatDistance(
-          props.loc.distanceFromUser,
-          "imperial",
-          props.loc.squareFootage,
-          "You are here?",
-        )
-      : "?";
-
-  const defaultColor = theme.colors.text;
-
   const content = props.other
-    ? <BevUiText
-        fontSize="massive"
-        iconSize="large"
-        preserveCase={true}
-        style={{ marginBottom: 5 }}
-        color={defaultColor}
-      >
+    ? <BevLargerTitleText>
         {`${props.index}. ${props.isLoading ? "" : "Other"}`}
-      </BevUiText>
+      </BevLargerTitleText>
     : <View>
-        <BevUiText
-          fontSize="massive"
-          iconSize="large"
-          preserveCase={true}
-          style={{ marginBottom: 5 }}
-          color={defaultColor}
-        >
+        <BevLargerTitleText>
           {titleText}
-        </BevUiText>
+        </BevLargerTitleText>
         {!props.isLoading
-          ? <View>
-              <BevUiText
-                icon="globe"
-                fontSize="large"
-                preserveCase={true}
-                style={{ marginBottom: 5 }}
-                color={defaultColor}
-              >
-                {addressText}
-              </BevUiText>
-              <BevUiText
-                icon="map-marker"
-                fontSize="large"
-                preserveCase={true}
-                color={
-                  props.isLoading
-                    ? defaultColor
-                    : props.loc
-                      ? CalcColorFromDistance(props.loc.distanceFromUser)
-                      : defaultColor
-                }
-              >
-                {distanceText}
-              </BevUiText>
-            </View>
+          ? <LocationAddressAndDistance
+              loc={props.loc}
+              paddingLeft={theme.padding.small}
+              calcColorFromDistance={true}
+            />
           : null}
       </View>;
 
@@ -107,7 +46,6 @@ const RedeemLocationChoiceLine: React.StatelessComponent<
     <BevPressableLine
       onPress={props.onPress}
       noHorizontalPadding={true}
-      showRightArrow={props.loc || props.other === true ? true : false}
       showLoading={props.isLoading ? true : false}
     >
       {content}
