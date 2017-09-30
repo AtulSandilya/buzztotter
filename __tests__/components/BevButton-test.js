58ad8959323b5cbfc533f670034ba73b1e6da0e2
Andrew Simms <simms.andrew@gmail.com>
2017-09-30 00:46:53 -0600

    Test: Update `BevButton` test

--- __tests__/components/BevButton-test.js
+++ __tests__/components/BevButton-test.js
@@ -7,7 +7,8 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   const tree = renderer.create(
     <BevButton
-      buttonText="Test"
+      text="Test"
+      shortText="Test"
       bevButtonPressed={() => {}}
     />
   ).toJSON();
@@ -18,25 +19,10 @@ it('renders correctly with button size', () => {
   const tree = renderer.create(
     <BevButton
       buttonFontSize={18}
-      buttonText="Test"
+      text="Test"
+      shortText="Test"
       bevButtonPressed={() => {}}
     />
   ).toJSON();
   expect(tree).toMatchSnapshot();
-});
-
-it('fails with no buttonText', () => {
-  const tree = renderer.create(
-    <BevButton
-      bevButtonPressed={() => {}}
-    />
-  ).toJSON();
-  expect(tree).toThrowErrorMatchingSnapshot();
-});
-
-it('fails with no props', () => {
-  const tree = renderer.create(
-    <BevButton/>
-  ).toJSON();
-  expect(tree).toThrowErrorMatchingSnapshot();
 });
