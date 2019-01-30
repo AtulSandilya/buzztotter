ed934d1bbaeeba1775539dace1e4bfa17d491a91
Atul <sandilyatul@gmail.com>
2019-01-31 03:27:54 +0530

    resolved common setup issues , working on android required setups

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,4 +1,4 @@
-const blacklist = require('metro-bundler/src/blacklist');
+// const blacklist = require('metro-bundler/src/blacklist');
 
 // Don't include server related files in the app bundle.
 // See https://github.com/facebook/react-native/issues/7271
