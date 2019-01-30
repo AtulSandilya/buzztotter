bcb6c128e491e8dae0d648bb2b9195118f212d79
Atul <sandilyatul@gmail.com>
2019-01-31 01:59:01 +0530

    first commit

--- ios/Bevegram/AppDelegate.m
+++ ios/Bevegram/AppDelegate.m
@@ -8,21 +8,14 @@
  */
 
 #import "AppDelegate.h"
-#import <Firebase.h>
-#import "RNFIRMessaging.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
-  [[FBSDKApplicationDelegate sharedInstance] application:application
-                           didFinishLaunchingWithOptions:launchOptions];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
@@ -38,44 +31,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
-  // Firebase setup from `react-native-firebase`
-  [FIRApp configure];
-  [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
-
   return YES;
 }
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-
-  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                                openURL:url
-                                                      sourceApplication:sourceApplication
-                                                             annotation:annotation
-                  ];
-  // Add any custom logic here.
-  return handled;
-}
-
-// Notification setup from `react-native-fcm`
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
- {
-   [RNFIRMessaging willPresentNotification:notification withCompletionHandler:completionHandler];
- }
-
- - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler
- {
-   [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
- }
-
- //You can skip this method if you don't want to use local notification
- -(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-   [RNFIRMessaging didReceiveLocalNotification:notification];
- }
-
- - (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
-   [RNFIRMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
- }
 
 @end
